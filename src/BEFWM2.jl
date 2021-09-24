6306718b47e949791d601b4c483ad14abec83e93
Eva Delmas <eva.delmas@gmail.com>
2021-09-24 11:02:34 -0400

    :heavy_plus_sign: DiffEqBase and :pencil2:

--- src/BEFWM2.jl
+++ src/BEFWM2.jl
@@ -3,19 +3,30 @@ module BEFWM2
 #dependencies
 using EcologicalNetworks
 using SparseArrays
+using DiffEqBase
 
 # Types
 include(joinpath(".", "types/declaration.jl"))
 include(joinpath(".", "types/typedisplay.jl"))
+
 include(joinpath(".", "inputs/foodwebs.jl"))
-include(joinpath(".", "measures/structure.jl"))
 include(joinpath(".", "inputs/functional_response.jl"))
 include(joinpath(".", "inputs/biological_rates.jl"))
 include(joinpath(".", "inputs/environment.jl"))
 
+include(joinpath(".", "model/productivity.jl"))
+include(joinpath(".", "model/consumption.jl"))
+include(joinpath(".", "model/metaboliclosses.jl"))
+include(joinpath(".", "model/model_parameters.jl"))
+include(joinpath(".", "model/dbdt.jl"))
+include(joinpath(".", "model/simulate.jl"))
 
-export FoodWeb, BEFWMParameters, FunctionalResponse, BioRates, Environment
+include(joinpath(".", "measures/structure.jl"))
+
+
+export FoodWeb, ModelParameters, FunctionalResponse, BioRates, Environment
 export homogeneous_preference, originalFR
 export allometricgrowth, allometricmetabolism, allometricmaxconsumption
+export simulate 
 
 end 
