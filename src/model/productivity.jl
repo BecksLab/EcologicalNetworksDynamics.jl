f650b6d40f90660be5571540389c358c722b0f12
Eva Delmas <eva.delmas@gmail.com>
2021-10-07 10:07:03 -0400

    :bug:

--- src/model/productivity.jl
+++ src/model/productivity.jl
@@ -2,7 +2,8 @@
 Productivity
 =#
 
-function basalgrowth(biomass, BR::BioRates, E::Environment)
-    G = 1 .- biomass ./ E.K
+function basalgrowth(biomass, FW::FoodWeb, BR::BioRates, E::Environment)
+    idp = _idproducers(FW.A)
+    G = (1 .- biomass ./ E.K) .* idp
     return BR.r .* G .* biomass 
 end
