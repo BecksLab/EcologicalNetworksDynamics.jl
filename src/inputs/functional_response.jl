87715df8f5b8587e78979f57721e0b5746ccfdae
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-01 11:22:24 +0200

    🎨 Autoformating function_response.jl

--- src/inputs/functional_response.jl
+++ src/inputs/functional_response.jl
@@ -5,27 +5,27 @@ Functional response
 """
     originalFW(FW; B0, hill_exponent, ω, interference)
 
-Returns an object of type FunctionalResponse where the functional response is the "original" 
+Returns an object of type FunctionalResponse where the functional response is the "original"
 as originally described by Yodzis and Innes.
 
-- FW is a FoodWeb object 
+- FW is a FoodWeb object
 - B0 (default is 0.5) is the half saturation density, it can be a single value (same for all consumers) or a vector of size S if you'd like consumer-specific values
 - hill_exponent (default is 2 / type III) describes the shape of the functional response (should be between 1 and 2)
 - ω (default is homogeneous preference) is a matrix describing cosnumers relative preference for their resources
-- interference (default is 0) is teh strength of predator interference in the model 
+- interference (default is 0) is teh strength of predator interference in the model
 """
 function originalFR(FW::FoodWeb,
-    ; B0::Union{Vector{T}, T}=0.5
-    , hill_exponent::T=2.0
-    , ω::Union{Nothing, Array{T,2}, SparseMatrixCSC{Float64,Int64}}=nothing
-    , interference::Union{T, Vector{T}}=0.0
-    , efficiency=nothing
-    , e_herbivore::T=0.45
-    , e_carnivore::T=0.85
-    ) where {T <: Real}
+    ; B0::Union{Vector{T},T}=0.5,
+    hill_exponent::T=2.0,
+    ω::Union{Nothing,Array{T,2},SparseMatrixCSC{Float64,Int64}}=nothing,
+    interference::Union{T,Vector{Real}}=0.0,
+    efficiency=nothing,
+    e_herbivore::T=0.45,
+    e_carnivore::T=0.85
+) where {T<:Real}
 
     S = richness(FW)
-    
+
     if length(interference) == 1
         interference = repeat([interference], S)
     else
@@ -35,7 +35,7 @@ function originalFR(FW::FoodWeb,
     if isnothing(ω)
         ω = homogeneous_preference(FW)
     else
-        isequal(S,S)(size(ω)) || throw(ArgumentError("The dimension of the relative preference matrix should be richness(FoodWeb)*richness(FoodWeb)"))
+        isequal(S, S)(size(ω)) || throw(ArgumentError("The dimension of the relative preference matrix should be richness(FoodWeb)*richness(FoodWeb)"))
     end
 
     if isnothing(efficiency)
@@ -44,7 +44,7 @@ function originalFR(FW::FoodWeb,
         if length(efficiency) == 1
             efficiency = FW.A .* efficiency
         else
-            isequal(S,S)(size(efficiency)) || throw(ArgumentError("The dimension of the assimilation efficiency matrix should be richness(FoodWeb)*richness(FoodWeb). Alternatively, you can provide a single value, or specify the e_herbivore and e_carnivore arguments."))
+            isequal(S, S)(size(efficiency)) || throw(ArgumentError("The dimension of the assimilation efficiency matrix should be richness(FoodWeb)*richness(FoodWeb). Alternatively, you can provide a single value, or specify the e_herbivore and e_carnivore arguments."))
             efficiency = sparse(efficiency)
         end
     end
@@ -59,16 +59,16 @@ function originalFR(FW::FoodWeb,
         S = length(FW.species)
         idx = findall(!iszero, FW.A)
         cons = unique([i[1] for i in idx])
-        FR = zeros(S,S)
+        FR = zeros(S, S)
 
         for c in cons
-            idxr = findall(!iszero, FW.A[c,:])
-            sumfoodavail = sum(ω[c,:] .* (B .^ hill_exponent))
+            idxr = findall(!iszero, FW.A[c, :])
+            sumfoodavail = sum(ω[c, :] .* (B .^ hill_exponent))
             for r in idxr
-                num = ω[c,r] * (B[r] ^ hill_exponent)
-                interf = interference[c] * B[r] * (B0[c] ^ hill_exponent) 
-                denom = (B0[c] ^ hill_exponent) + interf + sumfoodavail
-                FR[c,r] = num / denom
+                num = ω[c, r] * (B[r]^hill_exponent)
+                interf = interference[c] * B[r] * (B0[c]^hill_exponent)
+                denom = (B0[c]^hill_exponent) + interf + sumfoodavail
+                FR[c, r] = num / denom
             end
         end
 
@@ -86,8 +86,8 @@ function homogeneous_preference(FW::FoodWeb)
     idx = findall(!iszero, FW.A)
     for i in idx
         c = i[1]
-        nressource_c = sum(FW.A[c,:])
-        ω[i] = 1/nressource_c
+        nressource_c = sum(FW.A[c, :])
+        ω[i] = 1 / nressource_c
     end
     ω = sparse(ω)
 
@@ -96,19 +96,18 @@ end
 
 function assimilation_efficiency(FW::FoodWeb, e_herbivore, e_carnivore)
     S = richness(FW)
-    efficiency = zeros(Float64,(S, S))
+    efficiency = zeros(Float64, (S, S))
     idp = _idproducers(FW.A)
     for consumer in 1:S
         for resource in 1:S
-          if FW.A[consumer, resource]
-            if idp[resource]
-              efficiency[consumer, resource] = e_herbivore
-            else
-              efficiency[consumer, resource] = e_carnivore
+            if FW.A[consumer, resource]
+                if idp[resource]
+                    efficiency[consumer, resource] = e_herbivore
+                else
+                    efficiency[consumer, resource] = e_carnivore
+                end
             end
-          end
         end
-      end
-      return sparse(efficiency)
+    end
+    return sparse(efficiency)
 end
-
