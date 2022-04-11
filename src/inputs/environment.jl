d5d379e8adcfc1b059f1b2d36be0f0a3b8ccbf68
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-11 17:31:37 +0200

    🐛 Fixing compatibility with model_parameters

--- src/inputs/environment.jl
+++ src/inputs/environment.jl
@@ -3,12 +3,12 @@
 
 TODO
 """
-function Environment(FW::FoodWeb; K::Union{TP, Vector{TP}} = 1, T::TP = 293.15) where TP<:Real
+function Environment(FW::FoodWeb; K::Union{TP,Vector{TP}}=1, T::TP=293.15) where {TP<:Real}
     if isa(K, Vector)
         isequal(length(K))richness(FW) || throw(ArgumentError("K should be either a single value or a vector of length richness(FoodWeb)"))
     else
         K = repeat([K], richness(FW))
-        K[.!BEFWM2._idproducers(FW.A)] .= 0
+        K[.!whoisproducer(FW.A)] .= 0
     end
-    return Environment(K,T)
+    return Environment(K, T)
 end
