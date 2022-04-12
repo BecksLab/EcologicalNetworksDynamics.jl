b6bd3c0afc8097401c876e65b719e2ba54be118d
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-12 11:55:34 +0200

    🐛 Fixing compatibility with basal_growth
    basal_growth was still calling _idproducers which was removed

--- src/model/productivity.jl
+++ src/model/productivity.jl
@@ -3,7 +3,7 @@ Productivity
 =#
 
 function basalgrowth(biomass, FW::FoodWeb, BR::BioRates, E::Environment)
-    idp = _idproducers(FW.A)
+    idp = whoisproducer(FW.A)
     G = (1 .- biomass ./ E.K) .* idp
-    return BR.r .* G .* biomass 
+    return BR.r .* G .* biomass
 end
