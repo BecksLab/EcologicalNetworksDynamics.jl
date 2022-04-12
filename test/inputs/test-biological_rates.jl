97c7f223a6aa6321a86e236bdd08f1fe0ce1bf71
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-12 12:40:10 +0200

    ✨ Test: identifying metabolic classes

--- test/inputs/test-biological_rates.jl
+++ test/inputs/test-biological_rates.jl
@@ -1,7 +1,15 @@
 using Test
 using BEFWM2
 
-@testset """First test set""" begin
-    @test true == true
-    #fw = FoodWeb(nichemodel, 10, C=0.2, Z=10)
+A = [0 0 0 0; 0 0 0 0; 1 0 0 0; 0 1 0 0]
+foodweb = FoodWeb(A)
+foodweb.metabolic_class = ["producer", "producer", "invertebrate",
+    "ectotherm vertebrate"]
+foodweb.M = [1.0, 1.0, 10.0, 10.0]
+
+@testset "Identifying metabolic classes" begin
+    @test BEFWM2.whoisproducer(foodweb) == [1, 1, 0, 0]
+    @test BEFWM2.whoisproducer(foodweb.A) == [1, 1, 0, 0]
+    @test BEFWM2.whoisinvertebrate(foodweb) == [0, 0, 1, 0]
+    @test BEFWM2.whoisvertebrate(foodweb) == [0, 0, 0, 1]
 end
