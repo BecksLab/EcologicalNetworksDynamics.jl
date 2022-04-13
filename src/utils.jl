184ade304e80c8a3f23848600b4aaf86e91758c6
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-13 15:52:39 +0200

    🔧 Move whois functions to utils.jl
    Functions to identify metabolic classes belong to utils.jl

--- src/utils.jl
+++ src/utils.jl
@@ -1,1 +1,24 @@
 # Small functions useful for the whole package
+
+#### Identifying metabolic classes ####
+"Helper function called by `whois...` functions (e.g. `whoisproducer`)."
+function whois(metabolic_class::String, foodweb::FoodWeb)
+    vec(foodweb.metabolic_class .== metabolic_class)
+end
+"Which species is a producer or not? Return a BitVector."
+function whoisproducer(foodweb::FoodWeb)
+    whois("producer", foodweb)
+end
+"Which species is an vertebrate or not? Return a BitVector."
+function whoisvertebrate(foodweb::FoodWeb)
+    whois("ectotherm vertebrate", foodweb)
+end
+"Which species is an invertebrate or not? Return a BitVector."
+function whoisinvertebrate(foodweb::FoodWeb)
+    whois("invertebrate", foodweb)
+end
+
+function whoisproducer(A)
+    vec(.!any(A, dims=2))
+end
+#### end ####
