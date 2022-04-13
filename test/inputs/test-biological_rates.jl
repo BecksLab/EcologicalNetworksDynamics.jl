5174c3076abc9755dc9fd4ab4136621a051cc44c
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-13 10:15:20 +0200

    ✨ Test: helpers for allometric rate computation

--- test/inputs/test-biological_rates.jl
+++ test/inputs/test-biological_rates.jl
@@ -31,3 +31,12 @@ end
         0.88 * 10^-0.25]
     @test allometricrate(foodweb, DefaultMaxConsumptionParams()) == [0, 0, 8, 4]
 end
+
+@testset "Helper functions for allometric rate computation" begin
+    @test BEFWM2.allometricscale(0, 1, 1) == 0 # 0*1^1 = 0
+    @test BEFWM2.allometricscale(1, 2, 10) == 100 # 1*10^2 = 100
+    @test BEFWM2.allometricscale(2, 3, 2) == 16 # 2*2^3 = 16
+    expected_paramsvec = (a=[1, 1, 2, 3], b=[11, 11, 12, 13])
+    allometricparams = AllometricParams(1, 3, 2, 11, 13, 12)
+    @test BEFWM2.allometricparams_to_vec(foodweb, allometricparams) == expected_paramsvec
+end
