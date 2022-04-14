48e65bf1d97e3c0c2623a8989c0a438b62010d5d
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-14 17:20:56 +0200

    ✨ Test: classic FR parameters

--- test/inputs/test-functional_response.jl
+++ test/inputs/test-functional_response.jl
@@ -32,3 +32,24 @@ foodweb2 = FoodWeb(A2)
     @test Fbioenergetic_1.ω == sparse([0 0; 1 0])
     @test Fbioenergetic_2.ω == sparse([0 0 0; 1 0 0; 0.5 0.5 0])
 end
+
+@testset "Classic functional response parameters" begin
+    # Default
+    Fclassic_1 = ClassicResponse(foodweb1)
+    @test Fclassic_1.h == 2.0
+    @test Fclassic_1.aᵣ == sparse([0 0; 0.5 0])
+    @test Fclassic_1.hₜ == sparse([0 0; 1 0])
+    @test Fclassic_1.c == [0.0, 0.0]
+
+    # Custom
+    Fclassic_1 = ClassicResponse(foodweb1, h=1.0, aᵣ=[0 0; 0.2 0], hₜ=5, c=0.6)
+    @test Fclassic_1.h == 1.0
+    @test Fclassic_1.aᵣ == sparse([0 0; 0.2 0])
+    @test Fclassic_1.hₜ == sparse([0 0; 5 0])
+    @test Fclassic_1.c == [0.6, 0.6]
+
+    # Preferency matrix
+    Fclassic_2 = ClassicResponse(foodweb2)
+    @test Fclassic_1.ω == sparse([0 0; 1 0])
+    @test Fclassic_2.ω == sparse([0 0 0; 1 0 0; 0.5 0.5 0])
+end
