71cde8999fbc4d4e2a068c0cede46a59f79e5bc8
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-14 14:59:15 +0200

    🔧 Adding function to fill sparse matrix

--- src/utils.jl
+++ src/utils.jl
@@ -30,3 +30,35 @@ end
 function resourcenumber(consumer::Vector, foodweb::FoodWeb)
     Dict(i => resourcenumber(i, foodweb) for i in unique(consumer))
 end
+
+"""
+    scalar_to_sparsematrix(scalar, template_matrix)
+
+Return a matrix filled with a constant (`scalar`) for indexes where the value of the
+`template_matrix` is non-zero.
+
+# Examples
+
+```jldoctest
+julia> template_matrix = ones(2,2);
+
+julia> BEFWM2.scalar_to_sparsematrix(10, template_matrix)
+2×2 SparseArrays.SparseMatrixCSC{Float64, Int64} with 4 stored entries:
+ 10.0  10.0
+ 10.0  10.0
+
+julia> template_matrix[1,1] = 0;
+
+julia> BEFWM2.scalar_to_sparsematrix(10, template_matrix)
+2×2 SparseArrays.SparseMatrixCSC{Float64, Int64} with 3 stored entries:
+   ⋅   10.0
+ 10.0  10.0
+```
+"""
+function scalar_to_sparsematrix(scalar, template_matrix)
+    S = size(template_matrix, 1)
+    out_matrix = spzeros(S, S)
+    nonzero_indexes = findall(!iszero, template_matrix)
+    out_matrix[nonzero_indexes] .= scalar
+    sparse(out_matrix)
+end
