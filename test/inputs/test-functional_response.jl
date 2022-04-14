5969c9de8d7fc7e2317fffc0e6cfa274b5ed96dd
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-14 16:58:07 +0200

    ✨ Test: bioenergetic FR parameters

--- test/inputs/test-functional_response.jl
+++ test/inputs/test-functional_response.jl
@@ -8,3 +8,27 @@
     e_expect = sparse([0 0 0; 1 2 0; 1 2 2])
     @test BEFWM2.assimilation_efficiency(foodweb, e_herbivore=1, e_carnivore=2) == e_expect
 end
+
+A1 = [0 0; 1 0] # 2 eats 1
+A2 = [0 0 0; 1 0 0; 1 1 0] # 2 eats 1; 3 eats 1 & 2
+foodweb1 = FoodWeb(A1)
+foodweb2 = FoodWeb(A2)
+
+@testset "Bioenergetic functional response parameters" begin
+    # Default
+    Fbioenergetic_1 = BioenergeticResponse(foodweb1)
+    @test Fbioenergetic_1.B0 == [0.5, 0.5]
+    @test Fbioenergetic_1.h == 2.0
+    @test Fbioenergetic_1.c == [0.0, 0.0]
+
+    # Custom
+    Fbioenergetic_1 = BioenergeticResponse(foodweb1, B0=3.0, h=1.0, c=0.5)
+    @test Fbioenergetic_1.B0 == [3.0, 3.0]
+    @test Fbioenergetic_1.h == 1.0
+    @test Fbioenergetic_1.c == [0.5, 0.5]
+
+    # Preferency matrix
+    Fbioenergetic_2 = BioenergeticResponse(foodweb2)
+    @test Fbioenergetic_1.ω == sparse([0 0; 1 0])
+    @test Fbioenergetic_2.ω == sparse([0 0 0; 1 0 0; 0.5 0.5 0])
+end
