9280da026a53e80f9b7be8405491156132068ca1
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-14 12:51:45 +0200

    🔧 Functional response display

--- src/types/typedisplay.jl
+++ src/types/typedisplay.jl
@@ -1,5 +1,5 @@
 """
-Food Webs 
+Food Webs
 """
 
 function Base.show(io::IO, DFW::FoodWeb)
@@ -12,26 +12,22 @@ end
 Functional Response
 """
 
-function Base.show(io::IO, f::FunctionalResponse)
-    
-    str1 = "functional response: $(String(Symbol(f.functional_response)))"
-    if f.hill_exponent == 1.0
-        str2 = "type II"
-    elseif f.hill_exponent == 2.0 
-        str2 = "type III"
-    else
-        str2 = "h = $(f.hill_exponent)"
-    end
-    print(io, str1 * "\n" * str2)
-
+function Base.show(io::IO, F::ClassicResponse)
+    println(io, "Classic functional response")
+    print(io, "hill exponent = $(F.h)")
 end
 
-""" 
+function Base.show(io::IO, F::BioenergeticResponse)
+    println(io, "Bioenergetic functional response")
+    print(io, "hill exponent = $(F.h)")
+end
+
+"""
 Biological Rates
 """
 
 function Base.show(io::IO, b::BioRates)
-    
+
     str1 = "r (growth rate): $(b.r[1]), ..., $(b.r[end])"
     str2 = "x (metabolic rate): $(b.x[1]), ..., $(b.x[end])"
     str3 = "y (max. consumption rate): $(b.y[1]), ..., $(b.y[end])"
