a5598a882f58ae110e90684b9a23fd6207af183c
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-15 12:29:55 +0200

    🎨 Commenting dBdt!()

--- src/model/dbdt.jl
+++ src/model/dbdt.jl
@@ -6,14 +6,17 @@ function dBdt!(dB, B, Parameters::ModelParameters, t)
 
     B[B.<=0] .= 0 # ensuring non-negative biomass
 
+    # Define parameters
     foodweb = Parameters.FoodWeb
     biorates = Parameters.BioRates
     environment = Parameters.Environment
     F = Parameters.FunctionalResponse
 
+    # Compute equation terms
     growth = logisticgrowth(B, foodweb, biorates, environment)
     eating, being_eaten = consumption(B, foodweb, biorates, F, environment)
     metabolic_loss = metaboliclosses(B, biorates)
 
+    # Update dB/dt
     dB = growth .+ eating .- being_eaten .- metabolic_loss
 end
