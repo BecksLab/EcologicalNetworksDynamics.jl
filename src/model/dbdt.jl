52b5cf0506b6177f7f836cb6e5d39c9c14ca3cc1
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-15 12:12:42 +0200

    🔧 dBdt! modifies dB in place
    dBdt! does not need to return dB (!)

--- src/model/dbdt.jl
+++ src/model/dbdt.jl
@@ -2,7 +2,7 @@
 Core functions of the model
 =#
 
-function dBdt!(du, B, Parameters::ModelParameters, t)
+function dBdt!(dB, B, Parameters::ModelParameters, t)
 
     B[B.<=0] .= 0 # ensuring non-negative biomass
 
@@ -15,10 +15,5 @@ function dBdt!(du, B, Parameters::ModelParameters, t)
     eating, being_eaten = consumption(B, foodweb, biorates, F, environment)
     metabolic_loss = metaboliclosses(B, biorates)
 
-    dBdt = growth .+ eating .- being_eaten .- metabolic_loss
-    for i in eachindex(dBdt)
-        du[i] = dBdt[i] #can't return du directly, have to have 2 different objects dBdt and du for some reason...
-    end
-
-    return dBdt
+    dB = growth .+ eating .- being_eaten .- metabolic_loss
 end
