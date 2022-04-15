ee1b4da2121e06179ecc4595ad47cdb31c64142b
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-15 15:43:43 +0200

    🔧 Update consumption for BioRates.e

--- src/model/consumption.jl
+++ src/model/consumption.jl
@@ -10,12 +10,12 @@ function consumption(
     Environment::Environment
 )
 
-    x, y, e = biorates.x, biorates.y, F.Efficiency
+    x, y, e = biorates.x, biorates.y, biorates.e
     Fᵢⱼ = F(B)
     eating = x .* y .* B .* Fᵢⱼ
     being_eaten = (eating ./ e) .* foodweb.A
 
-    eating = vec(sum(eating, dims=2))
-    being_eaten = vec(sum(being_eaten, dims=1))
+    eating = vec(sum(eating, dims=2)) # sum on prey (i.e. columns)
+    being_eaten = vec(sum(being_eaten, dims=1)) # sum on predators (i.e. rows)
     eating, being_eaten
 end
