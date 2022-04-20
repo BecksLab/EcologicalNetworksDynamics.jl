23d338660f7f6071c9066a597936b3332d6496af
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-20 14:49:57 +0200

    🔧 Delete deprecated functions

--- src/model/consumption.jl
+++ src/model/consumption.jl
@@ -2,42 +2,6 @@
 Consumption
 =#
 
-function consumption(
-    B,
-    foodweb::FoodWeb,
-    biorates::BioRates,
-    F::BioenergeticResponse,
-    Environment::Environment
-)
-
-    x, y, e = biorates.x, biorates.y, biorates.e
-    Fᵢⱼ = F(B)
-    eating = x .* y .* B .* Fᵢⱼ
-    being_eaten = (eating ./ e) .* foodweb.A
-
-    eating = vec(sum(eating, dims=2)) # sum on prey (i.e. columns)
-    being_eaten = vec(sum(being_eaten, dims=1)) # sum on predators (i.e. rows)
-    eating, being_eaten
-end
-
-function consumption(
-    B,
-    foodweb::FoodWeb,
-    biorates::BioRates,
-    F::ClassicResponse,
-    Environment::Environment
-)
-
-    e = biorates.e
-    Fᵢⱼ = F(B)
-    being_eaten = B .* Fᵢⱼ
-    eating = e .* being_eaten
-
-    eating = vec(sum(eating, dims=2)) # sum on prey (i.e. columns)
-    being_eaten = vec(sum(being_eaten, dims=1)) # sum on predators (i.e. rows)
-    eating, being_eaten
-end
-
 function consumption(i, B, params::ModelParameters{BioenergeticResponse}, fᵣmatrix)
 
     # Set up
