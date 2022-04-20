fab3e616fee7fcbe0a5310389c2c8457408875bc
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-20 10:42:01 +0200

    🔧 Update logisticgrowth() and test (K:0->nothing)

--- test/model/test-productivity.jl
+++ test/model/test-productivity.jl
@@ -1,7 +1,7 @@
 @testset "Logistic growth" begin
     # Intern method
-    B, r, K = 1, 1, 0
-    @test BEFWM2.logisticgrowth(B, r, K) == 0 # K is null, growth is null
+    B, r, K = 1, 1, nothing
+    @test BEFWM2.logisticgrowth(B, r, K) == 0 # K is nothing, growth is null
     B, r, K = 1, 0, 1
     @test BEFWM2.logisticgrowth(B, r, K) == 0 # r is null, growth is null
     B, r, K = 0, 1, 1
