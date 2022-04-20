4f0488eb69ffdf857af6b3d93b2e66b2bf8517b7
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-20 16:17:59 +0200

    🔧 Updating tests for new methods

--- test/model/test-metabolic_loss.jl
+++ test/model/test-metabolic_loss.jl
@@ -2,7 +2,9 @@
     foodweb = FoodWeb([0 0 0; 0 0 0; 1 1 0])
     p = ModelParameters(foodweb)
     B = [1, 1, 1]
-    @test BEFWM2.metabolic_loss(B, p.BioRates) == [0, 0, 0.314]
+    @test BEFWM2.metabolic_loss(1, B, p) == 0 # no loss for producers
+    @test BEFWM2.metabolic_loss(2, B, p) == 0 # no loss for producers
+    @test BEFWM2.metabolic_loss(3, B, p) == 0.314 # loss for consumers...
     B = [2, 2, 2]
-    @test BEFWM2.metabolic_loss(B, p.BioRates) == [0, 0, 2 * 0.314]
+    @test BEFWM2.metabolic_loss(3, B, p) == 2 * 0.314 # ...increase with biomass
 end
