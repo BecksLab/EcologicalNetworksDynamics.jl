65021a08287fbe91ed605811f8b85ed832a2057b
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-04-20 16:17:59 +0200

    🔧 Updating tests for new methods

--- test/model/test-productivity.jl
+++ test/model/test-productivity.jl
@@ -14,9 +14,15 @@
     foodweb = FoodWeb([0 0 0; 0 0 0; 1 1 0]) # 1 & 2 producers
     p = ModelParameters(foodweb)
     B = [1, 1, 1]
-    @test BEFWM2.logisticgrowth(B, foodweb, p.BioRates, p.Environment) == [0, 0, 0]
+    @test BEFWM2.logisticgrowth(1, B, p) == 0
+    @test BEFWM2.logisticgrowth(2, B, p) == 0
+    @test BEFWM2.logisticgrowth(3, B, p) == 0
     B = [0.5, 0.5, 0.5]
-    @test BEFWM2.logisticgrowth(B, foodweb, p.BioRates, p.Environment) == [0.25, 0.25, 0]
+    @test BEFWM2.logisticgrowth(1, B, p) == 0.25
+    @test BEFWM2.logisticgrowth(2, B, p) == 0.25
+    @test BEFWM2.logisticgrowth(3, B, p) == 0
     p = ModelParameters(foodweb, BioRates=BioRates(foodweb, r=2))
-    @test BEFWM2.logisticgrowth(B, foodweb, p.BioRates, p.Environment) == [0.5, 0.5, 0]
+    @test BEFWM2.logisticgrowth(1, B, p) == 0.5
+    @test BEFWM2.logisticgrowth(2, B, p) == 0.5
+    @test BEFWM2.logisticgrowth(3, B, p) == 0
 end
