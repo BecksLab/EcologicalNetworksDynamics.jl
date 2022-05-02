1ccfceb273558cf7e06c31d5743d1a5301ab1790
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-05-02 16:40:16 +0200

    🔧 Refactor `simulate()`
    Output directly the output of solve()

--- src/model/simulate.jl
+++ src/model/simulate.jl
@@ -4,33 +4,28 @@ Simulations of biomass dynamics
 
 function simulate(
     params::ModelParameters,
-    B0;
-    start::Int64=0,
-    stop::Int64=500,
+    B0::AbstractVector;
+    start::Number=0,
+    stop::Number=500,
     use::Symbol=:nonstiff,
-    extinction_threshold::Float64=1e-6,
+    extinction_threshold::AbstractFloat=1e-6,
     δt::Number=0.25
 )
 
-    # Tests and format
+    # Check for consistency and format input arguments
     S = richness(params.network)
-    length(B0) ∈ [1, S] || throw(ArgumentError("B0 should be of length 1 or S
-        (species richeness)."))
-    length(B0) == S || (B0 = repeat([B0], S))
-    start < stop || throw(ArgumentError("'start' should be smaller than 'stop'."))
+    length(B0) ∈ [1, S] || throw(ArgumentError("B0 of size $(length(B0)) instead of $S:
+        B0 should be of length 1 or S (species richness)."))
+    length(B0) == S || (B0 = repeat(B0, S))
+    start < stop || throw(ArgumentError("'start' ($start) should be smaller than
+        'stop' ($stop)."))
     use ∈ vec([:stiff :nonstiff]) || throw(ArgumentError("'use' should be '::stiff'
         or '::nonstiff'."))
     algorithm = use == :stiff ? Rodas4(autodiff=false) : Tsit5()
 
-    # Pre-allocate the timeseries matrix
+    # Define ODE problem and solve
     timespan = (float(start), float(stop))
     timesteps = collect(start:δt:stop)
-
-    # Perform the actual integration
     problem = ODEProblem(dBdt!, B0, timespan, params)
-    sol = solve(problem, algorithm, saveat=timesteps)
-
-    # Output
-    B_trajectory = hcat(sol.u...)'
-    (ModelParameters=params, t=sol.t, B=B_trajectory)
+    solve(problem, algorithm, saveat=timesteps)
 end
