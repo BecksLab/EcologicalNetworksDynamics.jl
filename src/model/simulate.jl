d24586a2f0cd568035dac0735fc11ecb94f853ee
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-05-02 17:17:32 +0200

    📝 Write `simulate()` docstring

--- src/model/simulate.jl
+++ src/model/simulate.jl
@@ -2,6 +2,51 @@
 Simulations of biomass dynamics
 =#
 
+"""
+    simulate(
+        params,
+        B0;
+        start=0,
+        stop=500,
+        use=:nonstiff,
+        extinction_threshold=1e-6,
+        δt=0.25
+    )
+
+Run biomass dynamics simulation,
+given model parameters (`params`) and the initial biomass (`B0`).
+
+The dynamic is solved between t=`start` and t=`stop`.
+Biomass trajectories are saved every `δt`.
+
+The output of this function is the result of `DifferentialEquations.solve()`,
+to learn how to handle this output
+see [Solution Handling](https://diffeq.sciml.ai/stable/basics/solution/).
+
+# Examples
+```jldoctest
+julia> foodweb = FoodWeb([0 0; 1 0]); # create foodweb
+
+julia> params = ModelParameters(foodweb); # generate its parameters
+
+julia> B0 = [0.5, 0.5]; # set initial biomass
+
+julia> solution = simulate(params, B0); # run simulation
+
+julia> solution.t == collect(0:0.25:500) # saved timesteps
+true
+
+julia> solution[begin] # initial biomass, recover B0
+2-element Vector{Float64}:
+ 0.5
+ 0.5
+
+julia> round.(solution[end], digits=2) # final biomass
+2-element Vector{Float64}:
+ 0.19
+ 0.22
+```
+"""
 function simulate(
     params::ModelParameters,
     B0::AbstractVector;
