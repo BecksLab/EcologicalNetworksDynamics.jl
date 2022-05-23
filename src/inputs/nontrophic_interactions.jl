27e47caefc784f659c69dd337d258d759441519a
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-05-23 15:15:31 +0200

    ✨ Sample potential links randomly

--- src/inputs/nontrophic_interactions.jl
+++ src/inputs/nontrophic_interactions.jl
@@ -6,3 +6,23 @@ function potential_facilitation_links(foodweb)
     [(i, j) for i in (1:S), j in producers if i != j] # i facilitated, j facilitating
 end
 #### end ####
+
+#### Sample potential interactions ####
+"Draw randomly `L` links from the list of `potential_links`."
+function draw_links(potential_links, L::Integer)
+    Lmax = length(potential_links)
+    L >= 0 || throw(ArgumentError("L too small: should be positive."))
+    L <= Lmax || throw(ArgumentError("L too large: should be lower than $Lmax,
+    the maximum number of potential interactions."))
+    sample(potential_links, L, replace=false)
+end
+
+"Draw randomly from the list of `potential_links` s.t. the link connectance is `C`."
+function draw_links(potential_links, C::AbstractFloat)
+    0 <= C <= 1 || throw(ArgumentError("Connectance out of bounds: should be in [0,1]."))
+    Lmax = length(potential_links)
+    C >= 0.5 / Lmax || @warn "Low connectance: 0 link drawn."
+    L = round(Int64, C * Lmax)
+    draw_links(potential_links, L)
+end
+#### end ####
