9ed9fc77aed58ef755568fafd557ce7fbb9ac484
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-05-23 16:19:35 +0200

    ✨ Test: Sample potential links randomly

--- test/inputs/test-nontrophic_interactions.jl
+++ test/inputs/test-nontrophic_interactions.jl
@@ -12,3 +12,27 @@
     expect = Set([(1, 2), (2, 1), (1, 3), (3, 1), (2, 3), (3, 2), (4, 1), (4, 2), (4, 3)])
     @test Set(potential_facilitation_links(foodweb)) == expect
 end
+
+@testset "Non-trophic Interactions: draw randomly links." begin
+    foodweb = FoodWeb([0 0 0 0; 0 0 0 0; 0 0 0 0; 1 1 1 0]) # 1, 2 & 3 producers
+    potential_links = potential_facilitation_links(foodweb)
+    Lmax = length(potential_links)
+
+    # From number of links (L).
+    for L in 0:9
+        drawn_links = draw_links(potential_links, L)
+        @test length(drawn_links) == L
+        @test drawn_links ⊆ Set(potential_links)
+    end
+    @test_throws ArgumentError draw_links(potential_links, -1)
+    @test_throws ArgumentError draw_links(potential_links, Lmax + 1)
+
+    # From connectance (C).
+    for L in 1:9
+        drawn_links = draw_links(potential_links, L / Lmax)
+        @test length(drawn_links) == L
+        @test drawn_links ⊆ Set(potential_links)
+    end
+    @test_throws ArgumentError draw_links(potential_links, -0.1)
+    @test_throws ArgumentError draw_links(potential_links, 1.1)
+end
