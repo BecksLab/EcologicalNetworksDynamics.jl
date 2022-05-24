64161a689d9f69afe3241298e497fa0a430f84c8
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-05-24 14:54:07 +0200

    ✨ Find potential competition interactions + test

--- test/inputs/test-nontrophic_interactions.jl
+++ test/inputs/test-nontrophic_interactions.jl
@@ -11,6 +11,10 @@
     foodweb = FoodWeb([0 0 0 0; 0 0 0 0; 0 0 0 0; 1 1 1 0]) # 1, 2 & 3 producers
     expect = Set([(1, 2), (2, 1), (1, 3), (3, 1), (2, 3), (3, 2), (4, 1), (4, 2), (4, 3)])
     @test Set(potential_facilitation_links(foodweb)) == expect
+
+    # Competition.
+    expect = Set([(1, 2), (2, 1), (1, 3), (3, 1), (2, 3), (3, 2)])
+    @test Set(potential_competition_links(foodweb)) == expect
 end
 
 @testset "Non-trophic Interactions: draw randomly links." begin
