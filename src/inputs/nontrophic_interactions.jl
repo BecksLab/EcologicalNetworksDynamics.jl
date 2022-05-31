2bd1cb51f108e3e43deea1f4f37e361a3cbcd7c3
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-05-31 11:31:48 +0200

    🔧 Convert FoodWeb <-> MultiplexNetwork
    Overloading Base.convert
    + Union in FoodWeb methods is a dirty and temp fix

--- src/inputs/nontrophic_interactions.jl
+++ src/inputs/nontrophic_interactions.jl
@@ -50,6 +50,26 @@ function MultiplexNetwork(
 end
 #### end ####
 
+#### Conversion FoodWeb ↔ MultiplexNetwork ####
+import Base.convert
+
+"""
+Convert a [`MultiplexNetwork`](@ref) to a [`FoodWeb`](@ref).
+The convertion consists in removing the non-trophic layers of the multiplex networks.
+"""
+function convert(::Type{FoodWeb}, net::MultiplexNetwork)
+    FoodWeb(net.trophic, species=net.species_id, M=net.bodymass)
+end
+
+"""
+Convert a [`FoodWeb`](@ref) to a [`MultiplexNetwork`](@ref).
+The convertion consists in adding empty non-trophic layers to the foodweb.
+"""
+function convert(::Type{MultiplexNetwork}, net::FoodWeb)
+    MultiplexNetwork(net)
+end
+### end ####
+
 #### List potential interactions ####
 "Find potential facilitation links."
 function potential_facilitation_links(foodweb)
