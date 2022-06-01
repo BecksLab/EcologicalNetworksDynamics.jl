b187c52207b93ebbe699ee11310fc0a5af510946
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-06-01 14:11:42 +0200

    🔧 Create function to compute r_facilitated
    r_facilitated is the growth rate taking into account facilitat°

--- src/model/productivity.jl
+++ src/model/productivity.jl
@@ -13,3 +13,14 @@ function logisticgrowth(B, r, K)
     !isnothing(K) || return 0 # if carrying capacity is null, growth is null too (avoid NaNs)
     r * B * (1 - B / K)
 end
+
+"""
+Intrinsic growth rate of species i increased by facilitation.
+The new intrinsic growth rate `r_facilitated` is given by:
+``r'  = r (1 + f_0 \\sum_{k \\in \\{\\text{fac.}\\} B_k``
+"""
+function r_facilitated(r, i, B, network::MultiplexNetwork)
+    facilitating_species = network.facilitation[:, i]
+    f0 = network.nontrophic_intensity.f0
+    r * (1 + f0 * sum(B .* facilitating_species))
+end
