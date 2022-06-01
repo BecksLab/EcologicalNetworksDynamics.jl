f73073b3bd45c48f67389a306eca4af2ad889dbf
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-06-01 16:56:15 +0200

    🔧 Update tests to new logisticgrowth() methods

--- test/model/test-productivity.jl
+++ test/model/test-productivity.jl
@@ -13,16 +13,18 @@
     # Extern method
     foodweb = FoodWeb([0 0 0; 0 0 0; 1 1 0]) # 1 & 2 producers
     p = ModelParameters(foodweb)
+    K, r = p.environment.K, p.biorates.r
     B = [1, 1, 1]
-    @test BEFWM2.logisticgrowth(1, B, p) == 0
-    @test BEFWM2.logisticgrowth(2, B, p) == 0
-    @test BEFWM2.logisticgrowth(3, B, p) == 0
+    @test BEFWM2.logisticgrowth(1, B, r[1], K[1], foodweb) == 0
+    @test BEFWM2.logisticgrowth(2, B, r[2], K[2], foodweb) == 0
+    @test BEFWM2.logisticgrowth(3, B, r[3], K[3], foodweb) == 0
     B = [0.5, 0.5, 0.5]
-    @test BEFWM2.logisticgrowth(1, B, p) == 0.25
-    @test BEFWM2.logisticgrowth(2, B, p) == 0.25
-    @test BEFWM2.logisticgrowth(3, B, p) == 0
+    @test BEFWM2.logisticgrowth(1, B, r[1], K[1], foodweb) == 0.25
+    @test BEFWM2.logisticgrowth(2, B, r[2], K[2], foodweb) == 0.25
+    @test BEFWM2.logisticgrowth(3, B, r[3], K[3], foodweb) == 0
     p = ModelParameters(foodweb, biorates=BioRates(foodweb, r=2))
-    @test BEFWM2.logisticgrowth(1, B, p) == 0.5
-    @test BEFWM2.logisticgrowth(2, B, p) == 0.5
-    @test BEFWM2.logisticgrowth(3, B, p) == 0
+    K, r = p.environment.K, p.biorates.r
+    @test BEFWM2.logisticgrowth(1, B, r[1], K[1], foodweb) == 0.5
+    @test BEFWM2.logisticgrowth(2, B, r[2], K[2], foodweb) == 0.5
+    @test BEFWM2.logisticgrowth(3, B, r[3], K[3], foodweb) == 0
 end
