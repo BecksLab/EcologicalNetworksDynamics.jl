e2a32beef7213c8ff3304f062b198f92363625dd
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-06-01 14:31:48 +0200

    ✨ Facilitation activated in simulat° dynamics

--- src/model/dbdt.jl
+++ src/model/dbdt.jl
@@ -6,15 +6,18 @@ function dBdt!(dB, B, params::ModelParameters, t)
 
     B[B.<=0] .= 0 # ensuring non-negative biomass
 
-    # Set up
+    # Set up - Unpack parameters
     S = richness(params.network)
     fᵣmatrix = params.functional_response(B, params.network) # functional response matrix
+    r = params.biorates.r # vector of intrinsic growth rates
+    K = params.environment.K # vector of carrying capacities
+    network = params.network
 
     # Loop over species
     for i in 1:S
 
         # Compute ODE terms
-        growth = logisticgrowth(i, B, params)
+        growth = logisticgrowth(i, B, r[i], K[i], network)
         eating, being_eaten = consumption(i, B, params, fᵣmatrix)
         metabolism_loss = metabolic_loss(i, B, params)
 
