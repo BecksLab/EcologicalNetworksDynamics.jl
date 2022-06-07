b956732581f5783c8d8d4b9e30106a1aff95d0a5
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-06-07 16:37:41 +0200

    🔧 Improve naming and formating of response tests
    Add meaning to variable names to avoid var1, var2
    Cut text going over magin limits

--- test/inputs/test-functional_response.jl
+++ test/inputs/test-functional_response.jl
@@ -9,80 +9,80 @@
     @test BEFWM2.assimilation_efficiency(foodweb, e_herbivore=1, e_carnivore=2) == e_expect
 end
 
-A1 = [0 0; 1 0] # 2 eats 1
-A2 = [0 0 0; 1 0 0; 1 1 0] # 2 eats 1; 3 eats 1 & 2
-foodweb1 = FoodWeb(A1)
-multi_net1 = MultiplexNetwork(foodweb1)
-foodweb2 = FoodWeb(A2)
+A_2sp = [0 0; 1 0] # 2 eats 1
+A_3sp = [0 0 0; 1 0 0; 1 1 0] # 2 eats 1; 3 eats 1 & 2
+foodweb_2sp = FoodWeb(A_2sp)
+multi_net1 = MultiplexNetwork(foodweb_2sp)
+foodweb_3sp = FoodWeb(A_3sp)
 intensity = NonTrophicIntensity(0.0, 0.0, 0.0, 0.0)
-multi_net2 = MultiplexNetwork(foodweb2, intensity=intensity, C_interference=1.0)
+net_interference = MultiplexNetwork(foodweb_3sp, intensity=intensity, C_interference=1.0)
 
 @testset "Bioenergetic functional response parameters" begin
     # Default
-    Fbioenergetic_1 = BioenergeticResponse(foodweb1)
+    Fbioenergetic_1 = BioenergeticResponse(foodweb_2sp)
     @test Fbioenergetic_1.B0 == [0.5, 0.5]
     @test Fbioenergetic_1.h == 2.0
     @test Fbioenergetic_1.c == [0.0, 0.0]
 
     # Custom
-    Fbioenergetic_1 = BioenergeticResponse(foodweb1, B0=3.0, h=1.0, c=0.5)
+    Fbioenergetic_1 = BioenergeticResponse(foodweb_2sp, B0=3.0, h=1.0, c=0.5)
     @test Fbioenergetic_1.B0 == [3.0, 3.0]
     @test Fbioenergetic_1.h == 1.0
     @test Fbioenergetic_1.c == [0.5, 0.5]
 
     # Preferency matrix
-    Fbioenergetic_2 = BioenergeticResponse(foodweb2)
+    Fbioenergetic_2 = BioenergeticResponse(foodweb_3sp)
     @test Fbioenergetic_1.ω == sparse([0 0; 1 0])
     @test Fbioenergetic_2.ω == sparse([0 0 0; 1 0 0; 0.5 0.5 0])
 end
 
 @testset "Linear functional response parameters" begin
     # Default
-    Flinear_1 = LinearResponse(foodweb1)
+    Flinear_1 = LinearResponse(foodweb_2sp)
     @test Flinear_1.α == sparse([0, 1.0])
     @test Flinear_1.ω == sparse([0 0; 1.0 0.0])
 
     # Custom
-    Flinear_1 = LinearResponse(foodweb1, α=[0.0, 2.0])
+    Flinear_1 = LinearResponse(foodweb_2sp, α=[0.0, 2.0])
     @test Flinear_1.α == sparse([0, 2.0])
     @test Flinear_1.ω == sparse([0 0; 1.0 0.0])
 
-    # Custom and foodweb2
-    Flinear_2 = LinearResponse(foodweb2, α=3.0)
+    # Custom and foodweb_3sp
+    Flinear_2 = LinearResponse(foodweb_3sp, α=3.0)
     @test Flinear_2.α == sparse([0, 3.0, 3.0])
     @test Flinear_2.ω == sparse([0 0 0; 1 0 0; 0.5 0.5 0])
 end
 
 @testset "Classic functional response parameters" begin
     # Default
-    Fclassic_1 = ClassicResponse(foodweb1)
+    Fclassic_1 = ClassicResponse(foodweb_2sp)
     @test Fclassic_1.h == 2.0
     @test Fclassic_1.aᵣ == sparse([0 0; 0.5 0])
     @test Fclassic_1.hₜ == sparse([0 0; 1 0])
     @test Fclassic_1.c == [0.0, 0.0]
 
     # Custom
-    Fclassic_1 = ClassicResponse(foodweb1, h=1.0, aᵣ=[0 0; 0.2 0], hₜ=5, c=0.6)
+    Fclassic_1 = ClassicResponse(foodweb_2sp, h=1.0, aᵣ=[0 0; 0.2 0], hₜ=5, c=0.6)
     @test Fclassic_1.h == 1.0
     @test Fclassic_1.aᵣ == sparse([0 0; 0.2 0])
     @test Fclassic_1.hₜ == sparse([0 0; 5 0])
     @test Fclassic_1.c == [0.6, 0.6]
 
     # Preferency matrix
-    Fclassic_2 = ClassicResponse(foodweb2)
+    Fclassic_2 = ClassicResponse(foodweb_3sp)
     @test Fclassic_1.ω == sparse([0 0; 1 0])
     @test Fclassic_2.ω == sparse([0 0 0; 1 0 0; 0.5 0.5 0])
 end
 
 @testset "Bioenergetic functional response functor" begin
     # Index by index - FoodWeb
-    Fbioenergetic1_fw = BioenergeticResponse(foodweb1)
+    Fbioenergetic1_fw = BioenergeticResponse(foodweb_2sp)
     @test Fbioenergetic1_fw([1, 1], 1, 1) == 0 # no interaction
     @test Fbioenergetic1_fw([1, 1], 1, 2) == 0 # no interaction
     @test Fbioenergetic1_fw([1, 1], 2, 2) == 0 # no interaction
     @test Fbioenergetic1_fw([1, 1], 2, 1) == 1^2 / (0.5^2 + 1^2) # interaction
-    @test Fbioenergetic1_fw([1, 2], 2, 1) == 1^2 / (0.5^2 + 1^2) # don't depend on cons. mass
-    @test Fbioenergetic1_fw([2, 1], 2, 1) == 2^2 / (0.5^2 + 2^2) # ...but depend on res. mass
+    @test Fbioenergetic1_fw([1, 2], 2, 1) == 1^2 / (0.5^2 + 1^2) # don't depend on cons. B
+    @test Fbioenergetic1_fw([2, 1], 2, 1) == 2^2 / (0.5^2 + 2^2) # ...but depend on res. B
 
     # Index by index - FoodWeb
     Fbioenergetic1_nti = BioenergeticResponse(multi_net1)
@@ -90,59 +90,59 @@ end
     @test Fbioenergetic1_nti([1, 1], 1, 2) == 0 # no interaction
     @test Fbioenergetic1_nti([1, 1], 2, 2) == 0 # no interaction
     @test Fbioenergetic1_nti([1, 1], 2, 1) == 1^2 / (0.5^2 + 1^2) # interaction
-    @test Fbioenergetic1_nti([1, 2], 2, 1) == 1^2 / (0.5^2 + 1^2) # don't depend on cons. mass
-    @test Fbioenergetic1_nti([2, 1], 2, 1) == 2^2 / (0.5^2 + 2^2) # ...but depend on res. mass
+    @test Fbioenergetic1_nti([1, 2], 2, 1) == 1^2 / (0.5^2 + 1^2) # don't depend on cons. B
+    @test Fbioenergetic1_nti([2, 1], 2, 1) == 2^2 / (0.5^2 + 2^2) # ...but depend on res. B
 
     # Matrix
     F21 = 1^2 / (0.5^2 + 1^2)
     @test Fbioenergetic1_fw([1, 1]) == sparse([0 0; F21 0]) # provide biomass vector
-    @test Fbioenergetic1_fw([1, 1], foodweb1) == sparse([0 0; F21 0]) # method consistency
-    @test Fbioenergetic1_fw([1, 1], multi_net1) == sparse([0 0; F21 0]) # method consistency
-    @test Fbioenergetic1_fw(1) == sparse([0 0; F21 0]) # or a scalar if same for all species
+    @test Fbioenergetic1_fw([1, 1], foodweb_2sp) == sparse([0 0; F21 0]) # meth. consistency
+    @test Fbioenergetic1_fw([1, 1], multi_net1) == sparse([0 0; F21 0]) # meth. consistency
+    @test Fbioenergetic1_fw(1) == sparse([0 0; F21 0]) # or a scalar if same for all sp
 
     # Non-default hill exponent
-    Fbioenergetic_1 = BioenergeticResponse(foodweb1, h=3)
+    Fbioenergetic_1 = BioenergeticResponse(foodweb_2sp, h=3)
     F21 = 2^3 / (0.5^3 + 2^3)
     @test Fbioenergetic_1(2) == sparse([0 0; F21 0])
-    @test Fbioenergetic_1(2, foodweb1) == sparse([0 0; F21 0]) # method consistency
+    @test Fbioenergetic_1(2, foodweb_2sp) == sparse([0 0; F21 0]) # method consistency
     @test Fbioenergetic_1(2, multi_net1) == sparse([0 0; F21 0]) # nti=0 ⟺ foodweb
 
     # Consumer feeding on several resources
-    Fbioenergetic2_fw = BioenergeticResponse(foodweb2)
-    Fbioenergetic2_nti = BioenergeticResponse(multi_net2)
+    Fbioenergetic2_fw = BioenergeticResponse(foodweb_3sp)
+    Fbioenergetic2_nti = BioenergeticResponse(net_interference)
     B = [1, 1, 1] # uniform biomass distribution
     F21 = 1^2 / (0.5^2 + 1^2)
     F31 = 0.5 * 1^2 / ((0.5^2) + (0.5 * 1^2) + (0.5 * 1^2))
     F32 = F31
     @test Fbioenergetic2_fw(B) == sparse([0 0 0; F21 0 0; F31 F32 0])
     @test Fbioenergetic2_nti(B) == sparse([0 0 0; F21 0 0; F31 F32 0])
-    @test Fbioenergetic2_fw(B, foodweb2) == sparse([0 0 0; F21 0 0; F31 F32 0])
-    @test Fbioenergetic2_nti(B, multi_net2) == sparse([0 0 0; F21 0 0; F31 F32 0])
+    @test Fbioenergetic2_fw(B, foodweb_3sp) == sparse([0 0 0; F21 0 0; F31 F32 0])
+    @test Fbioenergetic2_nti(B, net_interference) == sparse([0 0 0; F21 0 0; F31 F32 0])
     B = [3, 2, 1] # non-uniform biomass distribution
     F21 = 3^2 / (0.5^2 + 3^2)
     F31 = 0.5 * 3^2 / ((0.5^2) + (0.5 * 3^2) + (0.5 * 2^2))
     F32 = 0.5 * 2^2 / ((0.5^2) + (0.5 * 3^2) + (0.5 * 2^2))
     @test Fbioenergetic2_fw(B) == sparse([0 0 0; F21 0 0; F31 F32 0])
     @test Fbioenergetic2_nti(B) == sparse([0 0 0; F21 0 0; F31 F32 0])
-    @test Fbioenergetic2_fw(B, foodweb2) == sparse([0 0 0; F21 0 0; F31 F32 0])
-    @test Fbioenergetic2_nti(B, multi_net2) == sparse([0 0 0; F21 0 0; F31 F32 0])
+    @test Fbioenergetic2_fw(B, foodweb_3sp) == sparse([0 0 0; F21 0 0; F31 F32 0])
+    @test Fbioenergetic2_nti(B, net_interference) == sparse([0 0 0; F21 0 0; F31 F32 0])
 
     # Adding intraspecific interference
-    Fbioenergetic2_fw = BioenergeticResponse(foodweb2, c=1)
-    Fbioenergetic2_nti = BioenergeticResponse(multi_net2, c=1)
+    Fbioenergetic2_fw = BioenergeticResponse(foodweb_3sp, c=1)
+    Fbioenergetic2_nti = BioenergeticResponse(net_interference, c=1)
     B = [3, 2, 1] # non-uniform biomass distribution
     F21 = 3^2 / (0.5^2 + 1 * 2 * 0.5^2 + 3^2)
     F31 = 0.5 * 3^2 / ((0.5^2) + (1 * 1 * 0.5^2) + (0.5 * 3^2 + 0.5 * 2^2))
     F32 = 0.5 * 2^2 / ((0.5^2) + (1 * 1 * 0.5^2) + (0.5 * 3^2 + 0.5 * 2^2))
     @test Fbioenergetic2_fw(B) == sparse([0 0 0; F21 0 0; F31 F32 0])
     @test Fbioenergetic2_nti(B) == sparse([0 0 0; F21 0 0; F31 F32 0])
-    @test Fbioenergetic2_fw(B, foodweb2) == sparse([0 0 0; F21 0 0; F31 F32 0])
-    @test Fbioenergetic2_nti(B, multi_net2) == sparse([0 0 0; F21 0 0; F31 F32 0])
+    @test Fbioenergetic2_fw(B, foodweb_3sp) == sparse([0 0 0; F21 0 0; F31 F32 0])
+    @test Fbioenergetic2_nti(B, net_interference) == sparse([0 0 0; F21 0 0; F31 F32 0])
 end
 
 @testset "Linear functional response functor" begin
     # Index by index - FoodWeb
-    Flinear1_fw = LinearResponse(foodweb1)
+    Flinear1_fw = LinearResponse(foodweb_2sp)
     @test Flinear1_fw([1, 1], 1, 1) == 0 # no interaction
     @test Flinear1_fw([1, 1], 1, 2) == 0 # no interaction
     @test Flinear1_fw([1, 1], 2, 2) == 0 # no interaction
@@ -161,21 +161,21 @@ end
 
     # Matrix
     @test Flinear1_fw([1, 1]) == sparse([0 0; 1.0 0]) # provide biomass vector
-    @test Flinear1_fw(1) == sparse([0 0; 1.0 0]) # or a scalar if same for all species
+    @test Flinear1_fw(1) == sparse([0 0; 1.0 0]) # or a scalar if same for all sp
     @test Flinear1_nti([1, 1]) == sparse([0 0; 1.0 0]) # provide biomass vector
-    @test Flinear1_nti(1) == sparse([0 0; 1.0 0]) # or a scalar if same for all species
-    @test Flinear1_nti([1, 1], foodweb1) == sparse([0 0; 1.0 0]) # provide biomass vector
-    @test Flinear1_nti([1, 1], multi_net1) == sparse([0 0; 1.0 0]) # or a scalar if same for all species
+    @test Flinear1_nti(1) == sparse([0 0; 1.0 0]) # or a scalar if same for all sp
+    @test Flinear1_nti([1, 1], foodweb_2sp) == sparse([0 0; 1.0 0]) # provide biomass vector
+    @test Flinear1_nti([1, 1], multi_net1) == sparse([0 0; 1.0 0]) # or a scalar
 
     # Non-default consumption rate
-    Flinear1_fw = LinearResponse(foodweb1, α=[0, 2.0])
+    Flinear1_fw = LinearResponse(foodweb_2sp, α=[0, 2.0])
     Flinear1_nti = LinearResponse(multi_net1, α=[0, 2.0])
     @test Flinear1_fw(2) == sparse([0 0; 4.0 0])
     @test Flinear1_nti(2) == sparse([0 0; 4.0 0])
 
     # Consumer feeding on several resources
-    Flinear2_fw = LinearResponse(foodweb2, α=[0, 1, 2])
-    Flinear2_nti = LinearResponse(multi_net2, α=[0, 1, 2])
+    Flinear2_fw = LinearResponse(foodweb_3sp, α=[0, 1, 2])
+    Flinear2_nti = LinearResponse(net_interference, α=[0, 1, 2])
     B = [3, 2, 1] # non-uniform biomass distribution
     @test Flinear2_fw(B) == sparse([0 0 0; 3 0 0; 3 2 0])
     @test Flinear2_nti(B) == sparse([0 0 0; 3 0 0; 3 2 0])
@@ -183,7 +183,7 @@ end
 
 @testset "Classic functional response functor" begin
     # Index by index - FoodWeb
-    Fclassic1_fw = ClassicResponse(foodweb1)
+    Fclassic1_fw = ClassicResponse(foodweb_2sp)
     @test Fclassic1_fw([1, 1], 1, 1) == 0 # no interaction
     @test Fclassic1_fw([1, 1], 1, 2) == 0 # no interaction
     @test Fclassic1_fw([1, 1], 2, 2) == 0 # no interaction
@@ -205,76 +205,77 @@ end
     @test Fclassic1_nti([2, 1], 2, 1) == F21_new # ...but depend on res. mass
 
     # Matrix
-    @test Fclassic1_fw([1, 1], foodweb1) == sparse([0 0; F21 0]) # provide biomass vector
-    @test Fclassic1_fw(1, foodweb1) == sparse([0 0; F21 0]) # or a scalar if same for all species
+    @test Fclassic1_fw([1, 1], foodweb_2sp) == sparse([0 0; F21 0]) # provide biomass vector
+    @test Fclassic1_fw(1, foodweb_2sp) == sparse([0 0; F21 0]) # or a scalar
     @test Fclassic1_nti([1, 1], multi_net1) == sparse([0 0; F21 0]) # provide biomass vector
-    @test Fclassic1_nti(1, multi_net1) == sparse([0 0; F21 0]) # or a scalar if same for all species
+    @test Fclassic1_nti(1, multi_net1) == sparse([0 0; F21 0]) # or a scalar
 
     # Non-default hill exponent
-    Fclassic_1 = ClassicResponse(foodweb1, h=3)
+    Fclassic_1 = ClassicResponse(foodweb_2sp, h=3)
     F21 = (1 * 0.5 * 2^3) / (1 + 0.5 * 1 * 2^3)
     @test Fclassic_1(2) == sparse([0 0; F21 0])
 
     # Non-default attack rate
-    Fclassic_1 = ClassicResponse(foodweb1, aᵣ=0.2)
+    Fclassic_1 = ClassicResponse(foodweb_2sp, aᵣ=0.2)
     F21 = (1 * 0.2 * 2^2) / (1 + 0.2 * 1 * 2^2)
     @test Fclassic_1(2) == sparse([0 0; F21 0])
 
     # Non-default handling time
-    Fclassic_1 = ClassicResponse(foodweb1, hₜ=2)
+    Fclassic_1 = ClassicResponse(foodweb_2sp, hₜ=2)
     F21 = (1 * 0.5 * 2^2) / (1 + 0.5 * 2 * 2^2)
     @test Fclassic_1(2) == sparse([0 0; F21 0])
 
 
     # Consumer feeding on several resources
-    Fclassic2_fw = ClassicResponse(foodweb2)
-    Fclassic2_nti = ClassicResponse(multi_net2)
+    Fclassic2_fw = ClassicResponse(foodweb_3sp)
+    Fclassic2_nti = ClassicResponse(net_interference)
     B = [1, 1, 1] # uniform biomass distribution
     F21 = (1 * 0.5 * 1^2) / (1 + 0.5 * 1 * 1^2)
     F31 = (0.5 * 0.5 * 1^2) / (1 + 0.5 * 0.5 * 1 * 1^2 + 0.5 * 0.5 * 1 * 1^2)
     F32 = F31
     @test Fclassic2_fw(B) == sparse([0 0 0; F21 0 0; F31 F32 0])
-    @test Fclassic2_nti(B, multi_net2) == sparse([0 0 0; F21 0 0; F31 F32 0])
+    @test Fclassic2_nti(B, net_interference) == sparse([0 0 0; F21 0 0; F31 F32 0])
     B = [3, 2, 1] # non-uniform biomass distribution
     F21 = (1 * 0.5 * 3^2) / (1 + 0.5 * 1 * 3^2)
     F31 = (0.5 * 0.5 * 3^2) / (1 + 0.5 * 0.5 * 1 * 3^2 + 0.5 * 0.5 * 1 * 2^2)
     F32 = (0.5 * 0.5 * 2^2) / (1 + 0.5 * 0.5 * 1 * 3^2 + 0.5 * 0.5 * 1 * 2^2)
     @test Fclassic2_fw(B) == sparse([0 0 0; F21 0 0; F31 F32 0])
-    @test Fclassic2_nti(B, multi_net2) == sparse([0 0 0; F21 0 0; F31 F32 0])
+    @test Fclassic2_nti(B, net_interference) == sparse([0 0 0; F21 0 0; F31 F32 0])
     B, aᵣ = [3, 2, 1], [0 0 0; 0.5 0 0; 0.5 0.2 0] # non-uniform biomass...
-    Fclassic2_fw = ClassicResponse(foodweb2, aᵣ=aᵣ) #...and non-uniform attack rate
-    Fclassic2_nti = ClassicResponse(multi_net2, aᵣ=aᵣ)
+    Fclassic2_fw = ClassicResponse(foodweb_3sp, aᵣ=aᵣ) #...and non-uniform attack rate
+    Fclassic2_nti = ClassicResponse(net_interference, aᵣ=aᵣ)
     F21 = (1 * 0.5 * 3^2) / (1 + 0.5 * 1 * 3^2)
     F31 = (0.5 * 0.5 * 3^2) / (1 + 0.5 * 0.5 * 1 * 3^2 + 0.5 * 0.2 * 1 * 2^2)
     F32 = (0.5 * 0.2 * 2^2) / (1 + 0.5 * 0.5 * 1 * 3^2 + 0.5 * 0.2 * 1 * 2^2)
     @test Fclassic2_fw(B) == sparse([0 0 0; F21 0 0; F31 F32 0])
-    @test Fclassic2_nti(B, multi_net2) == sparse([0 0 0; F21 0 0; F31 F32 0])
+    @test Fclassic2_nti(B, net_interference) == sparse([0 0 0; F21 0 0; F31 F32 0])
     B, hₜ = [3, 2, 1], [0 0 0; 0.9 0 0; 0.7 0.2 0] # non-uniform biomass...
-    Fclassic2_fw = ClassicResponse(foodweb2, hₜ=hₜ) #...and non-uniform handling time
-    Fclassic2_nti = ClassicResponse(multi_net2, hₜ=hₜ)
+    Fclassic2_fw = ClassicResponse(foodweb_3sp, hₜ=hₜ) #...and non-uniform handling time
+    Fclassic2_nti = ClassicResponse(net_interference, hₜ=hₜ)
     F21 = (1 * 0.5 * 3^2) / (1 + 0.5 * 0.9 * 3^2)
     F31 = (0.5 * 0.5 * 3^2) / (1 + 0.5 * 0.5 * 0.7 * 3^2 + 0.5 * 0.5 * 0.2 * 2^2)
     F32 = (0.5 * 0.5 * 2^2) / (1 + 0.5 * 0.5 * 0.7 * 3^2 + 0.5 * 0.5 * 0.2 * 2^2)
     @test Fclassic2_fw(B) ≈ sparse([0 0 0; F21 0 0; F31 F32 0]) atol = 1e-5
-    @test Fclassic2_nti(B, multi_net2) ≈ sparse([0 0 0; F21 0 0; F31 F32 0]) atol = 1e-5
+    expect = sparse([0 0 0; F21 0 0; F31 F32 0])
+    @test Fclassic2_nti(B, net_interference) ≈ expect atol = 1e-5
 
     # Adding intraspecific interference
-    Fclassic2_fw = ClassicResponse(foodweb2, c=1)
-    Fclassic2_nti = ClassicResponse(multi_net2, c=1)
+    Fclassic2_fw = ClassicResponse(foodweb_3sp, c=1)
+    Fclassic2_nti = ClassicResponse(net_interference, c=1)
     B = [3, 2, 1] # non-uniform biomass distribution
     F21 = (1 * 0.5 * 3^2) / (1 + 1 * 2 + 0.5 * 1 * 3^2)
     F31 = (0.5 * 0.5 * 3^2) / (1 + 1 * 1 + 0.5 * 0.5 * 1 * 3^2 + 0.5 * 0.5 * 1 * 2^2)
     F32 = (0.5 * 0.5 * 2^2) / (1 + 1 * 1 + 0.5 * 0.5 * 1 * 3^2 + 0.5 * 0.5 * 1 * 2^2)
     @test Fclassic2_fw(B) == sparse([0 0 0; F21 0 0; F31 F32 0])
-    @test Fclassic2_fw(B, foodweb2) == sparse([0 0 0; F21 0 0; F31 F32 0])
-    @test Fclassic2_nti(B, multi_net2) == sparse([0 0 0; F21 0 0; F31 F32 0])
+    @test Fclassic2_fw(B, foodweb_3sp) == sparse([0 0 0; F21 0 0; F31 F32 0])
+    @test Fclassic2_nti(B, net_interference) == sparse([0 0 0; F21 0 0; F31 F32 0])
 
     # Adding interspecific interference
-    multi_net2.nontrophic_intensity.i0 = 0.6 # activate interspecific interference
-    Fclassic2_nti = ClassicResponse(multi_net2, c=0.5) #! c=intraspecific interference
+    net_interference.nontrophic_intensity.i0 = 0.6 # activate interspecific interference
+    Fclassic2_nti = ClassicResponse(net_interference, c=0.5) #! c=intraspecific interference
     B = [3, 2, 1] # non-uniform biomass distribution
     F21 = (1 * 0.5 * 3^2) / (1 + 0.5 * 2 + 0.6 * 1 + 0.5 * 1 * 3^2)
     F31 = (0.5 * 0.5 * 3^2) / (1 + 0.5 * 1 + 0.6 * 2 + 0.5 * 0.5 * 1 * 3^2 + 0.5 * 0.5 * 1 * 2^2)
     F32 = (0.5 * 0.5 * 2^2) / (1 + 0.5 * 1 + 0.6 * 2 + 0.5 * 0.5 * 1 * 3^2 + 0.5 * 0.5 * 1 * 2^2)
-    @test Fclassic2_nti(B, multi_net2) == sparse([0 0 0; F21 0 0; F31 F32 0])
+    @test Fclassic2_nti(B, net_interference) == sparse([0 0 0; F21 0 0; F31 F32 0])
 end
