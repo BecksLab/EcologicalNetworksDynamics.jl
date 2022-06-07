39920f54a1513205637d76ea80fae04cae75c7e1
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-06-07 15:38:23 +0200

    ✨ Test: effect of facilitation on productivity

--- test/model/test-productivity.jl
+++ test/model/test-productivity.jl
@@ -10,7 +10,7 @@
     @test BEFWM2.logisticgrowth(B, r, K) == 0 # B = K, growth is null
     @test BEFWM2.logisticgrowth.(0.5, 1, 1) == 0.5 * 1 * (1 - 0.5 / 1)
 
-    # Extern method
+    # Extern method without facilitation
     foodweb = FoodWeb([0 0 0; 0 0 0; 1 1 0]) # 1 & 2 producers
     p = ModelParameters(foodweb)
     K, r = p.environment.K, p.biorates.r
@@ -27,6 +27,45 @@
     @test BEFWM2.logisticgrowth(1, B, r[1], K[1], foodweb) == 0.5
     @test BEFWM2.logisticgrowth(2, B, r[2], K[2], foodweb) == 0.5
     @test BEFWM2.logisticgrowth(3, B, r[3], K[3], foodweb) == 0
+
+    # Extern method with facilitation
+    multiplex_network = MultiplexNetwork(foodweb, C_facilitation=1.0)
+    # Facilitation to 0 <=> the growth is unchanged (compared to above section)
+    multiplex_network.nontrophic_intensity.f0 = 0.0
+    p = ModelParameters(multiplex_network)
+    K, r = p.environment.K, p.biorates.r
+    B = [1, 1, 1]
+    @test BEFWM2.logisticgrowth(1, B, r[1], K[1], multiplex_network) == 0
+    @test BEFWM2.logisticgrowth(2, B, r[2], K[2], multiplex_network) == 0
+    @test BEFWM2.logisticgrowth(3, B, r[3], K[3], multiplex_network) == 0
+    B = [0.5, 0.5, 0.5]
+    @test BEFWM2.logisticgrowth(1, B, r[1], K[1], multiplex_network) == 0.25
+    @test BEFWM2.logisticgrowth(2, B, r[2], K[2], multiplex_network) == 0.25
+    @test BEFWM2.logisticgrowth(3, B, r[3], K[3], multiplex_network) == 0
+    p = ModelParameters(multiplex_network, biorates=BioRates(multiplex_network, r=2))
+    K, r = p.environment.K, p.biorates.r
+    @test BEFWM2.logisticgrowth(1, B, r[1], K[1], multiplex_network) == 0.5
+    @test BEFWM2.logisticgrowth(2, B, r[2], K[2], multiplex_network) == 0.5
+    @test BEFWM2.logisticgrowth(3, B, r[3], K[3], multiplex_network) == 0
+    # Facilitation > 0 <=> the growth is changed (compared to above section)
+    for f0 in [1.0, 2.0, 5.0, 10.0]
+        multiplex_network.nontrophic_intensity.f0 = f0
+        p = ModelParameters(multiplex_network)
+        K, r = p.environment.K, p.biorates.r
+        B = [1, 1, 1]
+        @test BEFWM2.logisticgrowth(1, B, r[1], K[1], multiplex_network) == 0
+        @test BEFWM2.logisticgrowth(2, B, r[2], K[2], multiplex_network) == 0
+        @test BEFWM2.logisticgrowth(3, B, r[3], K[3], multiplex_network) == 0
+        B = [0.5, 0.5, 0.5]
+        @test BEFWM2.logisticgrowth(1, B, r[1], K[1], multiplex_network) == 0.25 * (1 + f0)
+        @test BEFWM2.logisticgrowth(2, B, r[2], K[2], multiplex_network) == 0.25 * (1 + f0)
+        @test BEFWM2.logisticgrowth(3, B, r[3], K[3], multiplex_network) == 0
+        p = ModelParameters(multiplex_network, biorates=BioRates(multiplex_network, r=2))
+        K, r = p.environment.K, p.biorates.r
+        @test BEFWM2.logisticgrowth(1, B, r[1], K[1], multiplex_network) == 0.5 * (1 + f0)
+        @test BEFWM2.logisticgrowth(2, B, r[2], K[2], multiplex_network) == 0.5 * (1 + f0)
+        @test BEFWM2.logisticgrowth(3, B, r[3], K[3], multiplex_network) == 0
+    end
 end
 
 @testset "Effect of facilitation on intrinsic growth rate" begin
