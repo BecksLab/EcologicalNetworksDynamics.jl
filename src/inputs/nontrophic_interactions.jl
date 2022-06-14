bcbaded6125db843a181ac566cb9cdfc4281c397
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-06-14 16:07:08 +0200

    ✨ Docstring and doctest for NTI features

--- src/inputs/nontrophic_interactions.jl
+++ src/inputs/nontrophic_interactions.jl
@@ -1,11 +1,20 @@
 #### Multiplex network objects ####
-"Layer"
+"""
+    Layer(adjacency::SparseMatrixCSC{Bool,Int64}, intensity::Union{Nothing,Float64})
+
+A `Layer` of an interaction type contains
+two pieces of information concerning this interaction:
+- `adjacency`: where the interactions occur given by the adjacency matrix
+- `intensity`: the intensity of the interaction
+
+The intensity is only defined for non-trophic interactions and is set to `nothing` for
+trophic interactions.
+"""
 mutable struct Layer
     adjacency::AdjacencyMatrix
     intensity::Union{Nothing,Float64}
 end
 
-"Multiplex network"
 mutable struct MultiplexNetwork <: EcologicalNetwork
     trophic_layer::Layer
     competition_layer::Layer
@@ -17,7 +26,104 @@ mutable struct MultiplexNetwork <: EcologicalNetwork
     metabolic_class::Vector{String}
 end
 
-"Build the [`MultiplexNetwork`](@ref) from the [`FoodWeb`](@ref)."
+"""
+    MultiplexNetwork(
+        foodweb::FoodWeb;
+        n_competition=0.0,
+        n_facilitation=0.0,
+        n_interference=0.0,
+        n_refuge=0.0,
+        A_competition=nontrophic_adjacency_matrix(foodweb, potential_competition_links,
+            n_competition, symmetric=true),
+        A_facilitation=nontrophic_adjacency_matrix(foodweb, potential_facilitation_links,
+            n_facilitation, symmetric=false),
+        A_interference=nontrophic_adjacency_matrix(foodweb, potential_interference_links,
+            n_interference, symmetric=true),
+        A_refuge=nontrophic_adjacency_matrix(foodweb, potential_refuge_links,
+            n_refuge, symmetric=false),
+        c0=1.0,
+        f0=1.0,
+        i0=1.0,
+        r0=1.0
+    )
+
+Build the `MultiplexNetwork` from a [`FoodWeb`](@ref).
+A multiplex is composed of a `trophic_layer` and several non-trophic [`Layer`](@ref)
+(`competition_layer`, `facilitation_layer`, `interference_layer` and `refuge_layer`).
+The `trophic_layer` is given by the [`FoodWeb`](@ref) and, by default,
+the non-trophic layers are assumed to be empty.
+To fill them 3 methods are possible.
+
+# Fill non-trophic layers
+
+First you can provide a number of links (should be an `Integer`).
+For instance to have two facilitation links you can specify `n_facilitation=2`.
+
+```jldoctest 1
+julia> foodweb = FoodWeb([0 0 0; 0 0 0; 1 1 0]); # 2 producers and 1 consumer
+
+julia> MultiplexNetwork(foodweb, n_facilitation=2)
+MultiplexNetwork of 3 species:
+  trophic_layer: 2 links
+  competition_layer: 0 links
+  facilitation_layer: 2 links
+  interference_layer: 0 links
+  refuge_layer: 0 links
+```
+
+Secondly, you can provide a connectance (should be an `AbstractFloat`).
+For instance, to fill as much as possible the competition layer you can specify
+`n_competition=1.0`.
+
+```jldoctest 1
+julia> MultiplexNetwork(foodweb, n_competition=1.0)
+MultiplexNetwork of 3 species:
+  trophic_layer: 2 links
+  competition_layer: 2 links
+  facilitation_layer: 0 links
+  interference_layer: 0 links
+  refuge_layer: 0 links
+```
+
+Lastly, you can provide a custom adjacency matrix.
+For instance, to fill the interference layer
+according to your custom adjacency matrix you can specify this matrix to `A_interference`.
+
+```jldoctest
+julia> foodweb = FoodWeb([0 0 0; 1 0 0; 1 0 0]); # 2 consumers feeding on producer 1
+
+julia> MultiplexNetwork(foodweb, A_interference=[0 0 0; 0 0 1; 0 1 0])
+MultiplexNetwork of 3 species:
+  trophic_layer: 2 links
+  competition_layer: 0 links
+  facilitation_layer: 0 links
+  interference_layer: 2 links
+  refuge_layer: 0 links
+```
+
+# Set non-trophic intensity
+
+The intensities of non-trophic interactions is governed by the four arguments:
+- `c0` for competition
+- `f0` for facilitation
+- `i0` for interference
+- `r0` for refuge
+
+By default all intensities are set to `1.0`.
+However, they can be easily modified by specifying a value to the adequate argument.
+For instance, if you want to set the facilitation intensity to `2.0`.
+
+```jldoctest
+julia> foodweb = FoodWeb([0 0 0; 0 0 0; 1 1 0]); # 2 producers and 1 consumer
+
+julia> multiplex_network = MultiplexNetwork(foodweb, n_facilitation=1, f0=2.0);
+
+julia> multiplex_network.facilitation_layer
+Layer(adjacency=AdjacencyMatrix(L=1), intensity=2.0)
+```
+
+See also [`FoodWeb`](@ref), [`Layer`](@ref).
+"""
 function MultiplexNetwork(
     foodweb::FoodWeb;
     n_competition=0.0,
@@ -135,30 +241,50 @@ end
 ### end ####
 
 #### List potential interactions ####
-"Find potential facilitation links."
-function potential_facilitation_links(foodweb)
+"""
+    potential_facilitation_links(foodweb::FoodWeb)
+
+Find possible facilitation links.
+Facilitation links can only occur from any species to a plant.
+"""
+function potential_facilitation_links(foodweb::FoodWeb)
     S = richness(foodweb)
     producers = (1:S)[whoisproducer(foodweb)]
     [(i, j) for i in (1:S), j in producers if i != j] # i facilitated, j facilitating
 end
 
-"Find potential competition links."
-function potential_competition_links(foodweb)
+"""
+    potential_competition_links(foodweb::FoodWeb)
+
+Find possible competition links.
+Competition links can only occur between two sessile species (producers).
+"""
+function potential_competition_links(foodweb::FoodWeb)
     S = richness(foodweb)
     producers = (1:S)[whoisproducer(foodweb)]
     [(i, j) for i in producers, j in producers if i != j]
 end
 
-"Find potential refuge links."
-function potential_refuge_links(foodweb)
+"""
+    potential_refuge_links(foodweb::FoodWeb)
+
+Find possible refuge links.
+Refuge links can only occur from a sessile species (producer) to a prey.
+"""
+function potential_refuge_links(foodweb::FoodWeb)
     S = richness(foodweb)
     producers = (1:S)[whoisproducer(foodweb)]
     preys = (1:S)[whoisprey(foodweb)]
     [(i, j) for i in producers, j in preys if i != j]
 end
 
-"Find potential interference links."
-function potential_interference_links(foodweb)
+"""
+    potential_interference_links(foodweb::FoodWeb)
+
+Find possible interference links.
+Interference liks can only occur between two predators sharing at least one prey.
+"""
+function potential_interference_links(foodweb::FoodWeb)
     S = richness(foodweb)
     predators = (1:S)[whoispredator(foodweb)]
     [(i, j) for i in predators, j in predators if i != j && share_prey(foodweb, i, j)]
@@ -167,6 +293,8 @@ end
 
 #### Sample potential interactions ####
 """
+    draw_asymmetric_links(potential_links, L::Integer)
+
 Draw randomly `L` links from the list of `potential_links`.
 Links are drawn asymmetrically,
 i.e. ``i`` interacts with ``j`` ⇏ ``j`` interacts with ``i``.
@@ -181,6 +309,8 @@ function draw_asymmetric_links(potential_links, L::Integer)
 end
 
 """
+    draw_asymmetric_links(potential_links, C::AbstractFloat)
+
 Draw randomly from the list of `potential_links` such that the link connectance is `C`.
 Links are drawn asymmetrically,
 i.e. ``i`` interacts with ``j`` ⇏ ``j`` interacts with ``i``.
@@ -194,6 +324,8 @@ function draw_asymmetric_links(potential_links, C::AbstractFloat)
 end
 
 """
+    draw_symmetric_links(potential_links, L::Integer)
+
 Draw randomly `L` links from the list of `potential_links`.
 Links are drawn symmetrically,
 i.e. ``i`` interacts with ``j`` ⇒ ``j`` interacts with ``i``.
@@ -214,6 +346,8 @@ function draw_symmetric_links(potential_links, L::Integer)
 end
 
 """
+    draw_symmetric_links(potential_links, C::AbstractFloat)
+
 Draw randomly from the list of `potential_links` such that the link connectance is `C`.
 Links are drawn symmetrically,
 i.e. ``i`` interacts with ``j`` ⇒ ``j`` interacts with ``i``.
@@ -228,18 +362,26 @@ function draw_symmetric_links(potential_links, C::AbstractFloat)
 end
 
 """
+    asymmetrize(V)
+
 Remove duplicate tuples from a symmetric vector of tuples.
 A vector `V` of tuples is said symmetric ⟺ ((i,j) ∈ `V` ⟺ (j,i) ∈ `V`).
 The tuple that has the 1st element inferior to its 2nd element is kept
 i.e. if i < j (i,j) is kept, and (j,i) otherwise.
+
+See also [`symmetrize`](@ref).
 """
 function asymmetrize(V)
     [(i, j) for (i, j) in V if i < j]
 end
 
 """
+    symmetrize(V)
+
 Add symmetric tuples from an asymmetric vector of tuples.
 A vector `V` of tuples is said asymmetric ⟺ ((i,j) ∈ `V` ⇒ (j,i) ∉ `V`).
+
+See also [`asymmetrize`](@ref).
 """
 function symmetrize(V)
     vcat(V, [(j, i) for (i, j) in V])
@@ -247,8 +389,26 @@ end
 #### end ####
 
 #### Generate the realized links ####
-"Generate the non-trophic matrix given the interaction number or connectance."
-function nontrophic_adjacency_matrix(foodweb, find_potential_links, n; symmetric=false)
+"""
+    nontrophic_adjacency_matrix(
+        foodweb::FoodWeb,
+        find_potential_links::Function,
+        n;
+        symmetric=false)
+
+Generate a non-trophic adjacency matrix
+given the function finding potential links (`find_potential_links`)
+and `n` which is interpreted as a number of links if `n:<Integer`
+or as a connectance if `n:<AbstractFloat`.
+
+See also [`potential_competition_links`](@ref), [`potential_facilitation_links`](@ref),
+[`potential_interference_links`](@ref), [`potential_refuge_links`](@ref).
+"""
+function nontrophic_adjacency_matrix(
+    foodweb::FoodWeb,
+    find_potential_links::Function,
+    n;
+    symmetric=false)
 
     # Initialization.
     S = richness(foodweb)
