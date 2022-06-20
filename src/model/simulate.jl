54b407bd3001b6fc4e27f1c922aa63bfc7616446
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-06-20 15:48:52 +0200

    🔧 `find_steady_state()` use `simulate()` for clarity
    Previous SteadyState solver had an obscure functioning.

--- src/model/simulate.jl
+++ src/model/simulate.jl
@@ -50,8 +50,8 @@ julia> round.(solution[end], digits=2) # final biomass
 function simulate(
     params::ModelParameters,
     B0::AbstractVector;
-    start::Number=0,
-    stop::Number=500,
+    t0::Number=0,
+    tmax::Number=500,
     alg_hints::Symbol=:auto,
     extinction_threshold::AbstractFloat=1e-6,
     δt::Number=0.25
@@ -62,8 +62,8 @@ function simulate(
     length(B0) ∈ [1, S] || throw(ArgumentError("B0 of size $(length(B0)) instead of $S:
         B0 should be of length 1 or S (species richness)."))
     length(B0) == S || (B0 = repeat(B0, S))
-    start < stop || throw(ArgumentError("'start' ($start) should be smaller than
-        'stop' ($stop)."))
+    t0 < tmax || throw(ArgumentError("'t0' ($t0) should be smaller than
+        'tmax' ($tmax)."))
 
     # Define callback - extinction threshold
     function species_under_threshold(u, t, integrator)
@@ -81,29 +81,38 @@ function simulate(
     positive_domain = PositiveDomain()
 
     # Define callback - terminate at steady state
-    terminate_steady_state = TerminateSteadyState(1e-6, 1e-4)
+    terminate_steady_state = TerminateSteadyState(1e-5, 1e-3)
 
     callbacks = CallbackSet(extinction_callback, positive_domain, terminate_steady_state)
 
     # Define ODE problem and solve
-    timespan = (float(start), float(stop))
-    timesteps = collect(start:δt:stop)
+    timespan = (float(t0), float(tmax))
+    timesteps = collect(t0:δt:tmax)
     problem = ODEProblem(dBdt!, B0, timespan, params)
     solve(problem, saveat=timesteps, alg_hints=[alg_hints], callback=callbacks)
 end
 
+"""
+    find_steady_state(params::ModelParameters, B0::AbstractVector)
+
+Find a steady state of a system parametrized by a parameter set `params`
+and some initial conditions `B0`.
+The function returns a tuple (`steady_state`, `converged`).
+If no steady state has been found `converged` is set to `false`
+and `steady_state` to `nothing`.
+Otherwise, `converged` is set to `true`
+and `steady_state` to the corresponding value.
+"""
 function find_steady_state(
     params::ModelParameters,
     B0::AbstractVector;
+    tmax=500,
+    kwargs...
 )
-
-    # Check for consistency and format input arguments
-    S = richness(params.network)
-    length(B0) ∈ [1, S] || throw(ArgumentError("B0 of size $(length(B0)) instead of $S:
-        B0 should be of length 1 or S (species richness)."))
-    length(B0) == S || (B0 = repeat(B0, S))
-
-    # Define ODE problem and solve
-    problem = SteadyStateProblem(dBdt!, B0, params)
-    solve(problem, SSRootfind())
+    out = simulate(params, B0; tmax=tmax, kwargs...)
+    converged = has_terminated(out, tmax)
+    steady_state = converged ? out.u[end] : nothing
+    (steady_state=steady_state, converged=converged)
 end
+
+has_terminated(out, tmax) = out.t[end] < tmax
