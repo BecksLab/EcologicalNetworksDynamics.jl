b2e06f63f680de4b1104e08f97e7fb6a222deea5
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-06-20 18:26:35 +0200

    🔧 Update `test-simulate`

--- test/model/test-simulate.jl
+++ test/model/test-simulate.jl
@@ -6,22 +6,22 @@
 
     # Solution converges
     solution1 = simulate(params, [0.5, 0.5])
-    @test solution1.retcode == :Success
-    solution2 = simulate(params, [0.3, 0.3], stop=1000)
+    @test solution1.retcode == :Terminated
+    solution2 = simulate(params, [0.3, 0.3], tmax=10)
     @test solution2.retcode == :Success
-    solution3 = simulate(params, [0.0, 0.0], δt=0.5)
+    solution3 = simulate(params, [0.2, 0.2], δt=0.5, tmax=10)
     @test solution3.retcode == :Success
 
     # Initial biomass
     @test solution1.u[begin] == [0.5, 0.5]
     @test solution2.u[begin] == [0.3, 0.3]
-    @test solution3.u[begin] == [0.0, 0.0]
+    @test solution3.u[begin] == [0.2, 0.2]
 
     # Timesteps
-    @test solution1.t == collect(0:0.25:500)
-    @test solution2.t == collect(0:0.25:1000)
-    @test solution3.t == collect(0:0.5:500)
+    @test all([t ∈ Set(solution2.t) for t in (0:0.25:10)])
+    @test all([t ∈ Set(solution3.t) for t in (0:0.5:10)])
 
     # If biomass start at 0, biomass stay at 0
-    @test all(hcat(solution3.u...) .== 0)
+    solution_null = simulate(params, [0.0, 0.0])
+    @test all(hcat(solution_null.u...) .== 0)
 end
