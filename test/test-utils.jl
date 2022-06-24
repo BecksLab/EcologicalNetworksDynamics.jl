1eeee836c869cb55533a09a3ca2fbe578ce3cfed
Ismaël Lajaaiti <ismael.lajaaiti@ens-lyon.fr>
2022-06-24 14:05:05 +0200

    🎨 Cleaning `test-utils.jl`

--- test/test-utils.jl
+++ test/test-utils.jl
@@ -1,43 +1,85 @@
+#### Set up food webs for tests ####
 foodweb_2links = FoodWeb([0 0 0 0; 0 0 0 0; 1 0 0 0; 0 1 0 0])
+foodweb_3links = FoodWeb([0 0 0 0; 0 0 0 0; 1 1 0 0; 0 0 1 0])
+foodweb_5links = FoodWeb([0 0 0 0; 1 0 0 0; 1 1 1 0; 0 0 1 0])
 foodweb_2links.metabolic_class = [
     "producer",
     "producer",
     "invertebrate",
     "ectotherm vertebrate"
 ]
-foodweb_3links = FoodWeb([0 0 0 0; 0 0 0 0; 1 1 0 0; 0 0 1 0])
-foodweb_5links = FoodWeb([0 0 0 0; 1 0 0 0; 1 1 1 0; 0 0 1 0])
+#### end ####
 
-@testset "Identify metabolic classes, preys and predators." begin
-    @test BEFWM2.producers(foodweb_2links) == [1, 2]
-    @test BEFWM2.invertebrates(foodweb_2links) == [3]
-    @test BEFWM2.vertebrates(foodweb_2links) == [4]
+@testset "Identify metabolic classes, producers, preys and predators" begin
+    # Function called on one species of the network (return a boolean)
+    @test BEFWM2.isinvertebrate(1, foodweb_2links) == false
+    @test BEFWM2.isinvertebrate(2, foodweb_2links) == false
+    @test BEFWM2.isinvertebrate(3, foodweb_2links) == true
+    @test BEFWM2.isinvertebrate(4, foodweb_2links) == false
+
+    @test BEFWM2.isvertebrate(1, foodweb_2links) == false
+    @test BEFWM2.isvertebrate(2, foodweb_2links) == false
+    @test BEFWM2.isvertebrate(3, foodweb_2links) == false
+    @test BEFWM2.isvertebrate(4, foodweb_2links) == true
 
     @test BEFWM2.isproducer(1, foodweb_2links) == true
     @test BEFWM2.isproducer(2, foodweb_2links) == true
     @test BEFWM2.isproducer(3, foodweb_2links) == false
     @test BEFWM2.isproducer(4, foodweb_2links) == false
 
-    @test BEFWM2.preys(foodweb_3links) == [1, 2, 3]
-    @test BEFWM2.predators(foodweb_3links) == [3, 4]
-    @test BEFWM2.preys(foodweb_2links) == [1, 2]
+    @test BEFWM2.ispredator(1, foodweb_2links) == false
+    @test BEFWM2.ispredator(2, foodweb_2links) == false
+    @test BEFWM2.ispredator(3, foodweb_2links) == true
+    @test BEFWM2.ispredator(4, foodweb_2links) == true
+
+    @test BEFWM2.isprey(1, foodweb_2links) == true
+    @test BEFWM2.isprey(2, foodweb_2links) == true
+    @test BEFWM2.isprey(3, foodweb_2links) == false
+    @test BEFWM2.isprey(4, foodweb_2links) == false
+
+    # Functions called on the whole network (return a vector of indexes)
+    @test BEFWM2.invertebrates(foodweb_2links) == [3]
+    @test BEFWM2.invertebrates(foodweb_2links) == [3]
+    @test BEFWM2.invertebrates(foodweb_2links) == [3]
+
+    @test BEFWM2.vertebrates(foodweb_2links) == [4]
+    @test BEFWM2.vertebrates(foodweb_2links) == [4]
+    @test BEFWM2.vertebrates(foodweb_2links) == [4]
+
+    @test BEFWM2.producers(foodweb_2links) == [1, 2]
+    @test BEFWM2.producers(foodweb_3links) == [1, 2]
+    @test BEFWM2.producers(foodweb_5links) == [1]
+
     @test BEFWM2.predators(foodweb_2links) == [3, 4]
+    @test BEFWM2.predators(foodweb_3links) == [3, 4]
+    @test BEFWM2.predators(foodweb_5links) == [2, 3, 4]
+
+    @test BEFWM2.preys(foodweb_2links) == [1, 2]
+    @test BEFWM2.preys(foodweb_3links) == [1, 2, 3]
+    @test BEFWM2.preys(foodweb_5links) == [1, 2, 3]
 end
 
-@testset "Find predators who share at least one prey." begin
-    expected = [
-        (1, 2, false),
-        (2, 1, false),
-        (3, 2, true),
-        (2, 3, true),
-        (2, 4, false),
-        (4, 2, false),
-        (3, 4, true),
-        (4, 3, true)
-    ]
-    for (i, j, e) in expected
-        @test BEFWM2.share_prey(i, j, foodweb_5links) == e
-    end
+@testset "Find predators and preys of a given species" begin
+    @test BEFWM2.preys_of(1, foodweb_5links) == []
+    @test BEFWM2.preys_of(2, foodweb_5links) == [1]
+    @test BEFWM2.preys_of(3, foodweb_5links) == [1, 2, 3]
+    @test BEFWM2.preys_of(4, foodweb_5links) == [3]
+
+    @test BEFWM2.predators_of(1, foodweb_5links) == [2, 3]
+    @test BEFWM2.predators_of(2, foodweb_5links) == [3]
+    @test BEFWM2.predators_of(3, foodweb_5links) == [3, 4]
+    @test BEFWM2.predators_of(4, foodweb_5links) == []
+end
+
+@testset "Find predators sharing at least one prey" begin
+    @test BEFWM2.share_prey(1, 2, foodweb_5links) == false
+    @test BEFWM2.share_prey(2, 1, foodweb_5links) == false
+    @test BEFWM2.share_prey(3, 2, foodweb_5links) == true
+    @test BEFWM2.share_prey(2, 3, foodweb_5links) == true
+    @test BEFWM2.share_prey(2, 4, foodweb_5links) == false
+    @test BEFWM2.share_prey(4, 2, foodweb_5links) == false
+    @test BEFWM2.share_prey(3, 4, foodweb_5links) == true
+    @test BEFWM2.share_prey(4, 3, foodweb_5links) == true
 end
 
 @testset "Find number of resources of each species." begin
