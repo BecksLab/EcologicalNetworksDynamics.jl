d48e45bb93104d59248c2eaac0ec06c544e03fbf
Iago-lito <iago-lito@etak>
2022-09-13 17:30:19 +0200

    Autoformat test-stochasticity.jl.

--- test/inputs/test-stochasticity.jl
+++ test/inputs/test-stochasticity.jl
@@ -1,16 +1,29 @@
 # Set up
-A = [0 0 0 0 0; 1 0 0 1 0; 0 0 0 0 0; 0 0 1 0 1; 0 0 0 0 0]
+A = [
+    0 0 0 0 0
+    1 0 0 1 0
+    0 0 0 0 0
+    0 0 1 0 1
+    0 0 0 0 0
+]
 input_σ = [0.1, 0.2]
 input_θ = [0.2, 0.5, 0.3, 0.4]
-foodweb = FoodWeb(A, Z = 10)
+foodweb = FoodWeb(A; Z = 10)
 producer = whoisproducer(A)
 biorates = BioRates(foodweb)
 
-stochasticity = AddStochasticity(foodweb, biorates, addstochasticity = true, 
-    wherestochasticity = "producers", nstochasticity = "all", σe = input_σ, θ = input_θ)
+stochasticity = AddStochasticity(
+    foodweb,
+    biorates;
+    addstochasticity = true,
+    wherestochasticity = "producers",
+    nstochasticity = "all",
+    σe = input_σ,
+    θ = input_θ,
+)
 
 @testset "Correct sampling of stochastic species" begin
-    @test stochasticity.stochspecies == findall(x->x==1, producer)
+    @test stochasticity.stochspecies == findall(x -> x == 1, producer)
     @test stochasticity.stochconsumers == []
     @test stochasticity.stochproducers == stochasticity.stochspecies
 end
@@ -23,15 +36,22 @@ end
 end
 
 # Adding stochasticity to specific species
-stochasticity = AddStochasticity(foodweb, biorates, addstochasticity = true, 
-    wherestochasticity = [2,3,4], nstochasticity = 3, σe = input_σ, θ = input_θ)
+stochasticity = AddStochasticity(
+    foodweb,
+    biorates;
+    addstochasticity = true,
+    wherestochasticity = [2, 3, 4],
+    nstochasticity = 3,
+    σe = input_σ,
+    θ = input_θ,
+)
 
 @testset "Correct sampling of stochastic species" begin
-    @test stochasticity.stochspecies == [2,3,4]
-    @test stochasticity.stochconsumers == [2,4]
+    @test stochasticity.stochspecies == [2, 3, 4]
+    @test stochasticity.stochconsumers == [2, 4]
     @test stochasticity.stochproducers == [3]
 end
 
 @testset "μ from correct BioRates arguments" begin
-    @test stochasticity.μ == [biorates.x[2],biorates.r[3],biorates.x[4]]
+    @test stochasticity.μ == [biorates.x[2], biorates.r[3], biorates.x[4]]
 end
