e6b00a2a8f6d17d39930871caafd7d1bc6dd96a7
Alain Danet <alain.danet@mnhn.fr>
2022-11-15 18:57:19 +0000

    Change ProducerCompetition() to handle a matrix.

--- test/inputs/test-producer_competition.jl
+++ test/inputs/test-producer_competition.jl
@@ -2,11 +2,25 @@
     foodweb = FoodWeb([0 0 0; 0 0 0; 0 1 0])
     compet = ProducerCompetition(foodweb) # default
     @test compet.α == [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 0.0]
-    @test compet.αii == 1.0
-    @test compet.αij == 0.0
     compet = ProducerCompetition(foodweb; αii = 1.0, αij = 1.0) # Put intercompetition
     # Competition terms are 0 for all αij involving non-producers 
     @test compet.α == [1.0 1.0 0.0; 1.0 1.0 0.0; 0.0 0.0 0.0]
-    @test compet.αii == 1.0
-    @test compet.αij == 1.0
+    # Test that custom matrices work
+    myα = [1.0 1.0 0.0; 1.0 1.0 0.0; 0.0 0.0 0.0]
+    compet = ProducerCompetition(foodweb; α = myα)
+    @test compet.α == [1.0 1.0 0.0; 1.0 1.0 0.0; 0.0 0.0 0.0]
+    # Should fail if non zero α for non-producers
+    @test_throws AssertionError("all(α[non_producer, :] .== 0)") ProducerCompetition(
+        foodweb,
+        α = [1.0 1.0 0.0; 1.0 1.0 0.0; 1.0 0.0 0.0],
+    )
+    # Should fail if the α matrix dim does not match the size of the foodweb 
+    @test_throws AssertionError("size(α, 1) == size(α, 2) == S") ProducerCompetition(
+        foodweb,
+        α = [1.0 0.0; 1.0 0.0; 0.0 0.0],
+    )
+    @test_throws AssertionError("size(α, 1) == size(α, 2) == S") ProducerCompetition(
+        foodweb,
+        α = [1.0 0.0; 1.0 0.0],
+    )
 end
