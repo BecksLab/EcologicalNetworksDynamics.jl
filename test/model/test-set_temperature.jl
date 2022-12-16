ceab42482a7e76719a198377e8b8798a100795f6
hanamayall <hana@hanachan.co.uk>
2022-12-16 16:31:01 +0000

    test TD

--- test/model/test-set_temperature.jl
+++ test/model/test-set_temperature.jl
@@ -16,26 +16,21 @@ temp = 303.15 # temperature in Kelvin
     @test p.temperature_response == NoTemperatureResponse()
 
     # MP with Classic Response 
-    p = ModelParameters(foodweb; functional_response = ClassicResponse(foodweb))
+    p = ModelParameters(foodweb, functional_response = ClassicResponse(foodweb))
 
     # Exponential Boltzmann-Arrhenius temperature dependence 
     set_temperature!(p, temp, ExponentialBA())
-    @test p.environment.T == temp
-    @test p.environment.K ==
-          exponentialBA_vector_rate(foodweb, temp, DefaultExpBACarryingCapacityParams())
-    @test p.biorates.r ==
-          exponentialBA_vector_rate(foodweb, temp, DefaultExpBAGrowthParams())
-    @test p.biorates.x ==
-          exponentialBA_vector_rate(foodweb, temp, DefaultExpBAMetabolismParams())
-    @test p.functional_response.hₜ ==
-          exponentialBA_matrix_rate(foodweb, temp, DefaultExpBAHandlingTimeParams())
-    @test p.functional_response.aᵣ ==
-          exponentialBA_matrix_rate(foodweb, temp, DefaultExpBAAttackRateParams())
+    @test p.environment.T == temptes
+    @test p.environment.K == exponentialBA_vector_rate(foodweb, temp, DefaultExpBACarryingCapacityParams())
+    @test p.biorates.r == exponentialBA_vector_rate(foodweb, temp, DefaultExpBAGrowthParams())
+    @test p.biorates.x == exponentialBA_vector_rate(foodweb, temp, DefaultExpBAMetabolismParams())
+    @test p.functional_response.hₜ == exponentialBA_matrix_rate(foodweb, temp, DefaultExpBAHandlingTimeParams())
+    @test p.functional_response.aᵣ == exponentialBA_matrix_rate(foodweb, temp, DefaultExpBAAttackRateParams())
     @test typeof(p.temperature_response) == ExponentialBA
     @test p.temperature_response.r == DefaultExpBAGrowthParams()
     @test p.temperature_response.x == DefaultExpBAMetabolismParams()
     @test p.temperature_response.aᵣ == DefaultExpBAAttackRateParams()
     @test p.temperature_response.hₜ == DefaultExpBAHandlingTimeParams()
     @test p.temperature_response.K == DefaultExpBACarryingCapacityParams()
-
+    
 end
