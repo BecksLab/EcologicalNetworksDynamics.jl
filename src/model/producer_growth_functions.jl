c5f10254952b040c797c6ddff2673c80d27ec099
Ismaël Lajaaiti <ismael.lajaaiti@gmail.com>
2023-04-28 16:28:48 +0200

    Clean files

--- src/model/producer_growth_functions.jl
+++ src/model/producer_growth_functions.jl
@@ -1,3 +1,4 @@
+# Producer growth functors.
 function (g::LogisticGrowth)(i, u, params::ModelParameters)
     isnothing(g.K[i]) && return 0.0 # Species i is not a producer.
     B = u[species(params)]
@@ -82,4 +83,27 @@ function growth(parms::ModelParameters, ::Symbol)
     ]
 
     code, data
+end
+
+"""
+    nutrient_dynamics(p::ProducerGrowth, i, u, G, network::EcologicalNetwork)
+
+Compute the dynamics of the nutrient `i` given the biomass `B`,
+the nutrients abundances `N` and the vector of species growths `G`.
+
+The nutrient dynamics is on only if `p` is of type `NutrientIntake`.
+"""
+function nutrient_dynamics(model::ModelParameters, u, i, G)
+    p = model.producer_growth
+    if isa(p, LogisticGrowth)
+        throw(ArgumentError("Nutrient dynamics cannot be computed for producer growth \
+                            of type `LogisticGrowth`."))
+    end
+    B = u[species(model)] # Species biomass.
+    N = u[nutrients(model)] # Nutrient abundances.
+    nutrient_idx = findfirst(==(i), nutrients(model)) # Index of the nutrient `i`.
+    d = p.turnover[nutrient_idx]
+    s = p.supply[nutrient_idx]
+    c = p.concentration[:, nutrient_idx]
+    d * (s - N[nutrient_idx]) - sum(c .* G .* B)
 end
